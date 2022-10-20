962a96991b29d65edfe57f3b549a3e59db5a3daf
David Edler <david.edler@canonical.com>
2022-10-20 12:41:13 +0200

    fix the in browser terminal

--- static/js/InstanceTerminal.tsx
+++ static/js/InstanceTerminal.tsx
@@ -1,21 +1,29 @@
-import React, { FC, useState } from "react";
+import React, { FC, useLayoutEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { XTerm } from "xterm-for-react";
 import Xterm from "xterm-for-react/dist/src/XTerm";
+import { FitAddon } from "xterm-addon-fit";
 import { fetchInstanceExec } from "./api/instances";
 import { Row } from "@canonical/react-components";
+import NotificationRow from "./NotificationRow";
+import { Notification } from "./types/notification";
+import { getWsErrorMsg } from "./helpers";
 
 type Params = {
   name: string;
 };
 
 const InstanceTerminal: FC = () => {
-  const [input, setInput] = useState("");
   const { name } = useParams<Params>();
   const xtermRef = React.useRef<Xterm>(null);
-  const [ws, setWs] = useState<WebSocket | null>(null);
+  const textEncoder = new TextEncoder();
+  const [dataWs, setDataWs] = useState<WebSocket | null>(null);
+  const [controlWs, setControlWs] = useState<WebSocket | null>(null);
+  const [notification, setNotification] = useState<Notification | null>(null);
 
-  const loadConsole = async () => {
+  const [fitAddon] = useState<FitAddon>(new FitAddon());
+
+  const openWebsockets = async () => {
     if (!name) {
       return;
     }
@@ -26,44 +34,89 @@ const InstanceTerminal: FC = () => {
     const result = await fetchInstanceExec(name);
 
     const dataUrl = `wss://0.0.0.0:8443${result.operation}/websocket?secret=${result.metadata.metadata.fds["0"]}`;
-    const cmdUrl = `wss://0.0.0.0:8443${result.operation}/websocket?secret=${result.metadata.metadata.fds["control"]}`;
+    const controlUrl = `wss://0.0.0.0:8443${result.operation}/websocket?secret=${result.metadata.metadata.fds["control"]}`;
 
-    const dataSocket = new WebSocket(dataUrl);
-    const controlSocket = new WebSocket(cmdUrl);
+    const data = new WebSocket(dataUrl);
+    const control = new WebSocket(controlUrl);
 
-    controlSocket.onopen = (event) => {
-      console.log("control open", event);
+    control.onopen = () => {
+      setControlWs(control);
     };
 
-    controlSocket.onerror = (event) => {
-      console.log("control error", event);
+    control.onerror = () => {
+      setNotification({
+        message: "There was an error with control websocket",
+        type: "negative",
+      });
     };
 
-    controlSocket.onmessage = (message) => {
+    control.onclose = (event) => {
+      setNotification({
+        message: getWsErrorMsg(event.code),
+        type: "negative",
+      });
+    };
+
+    control.onmessage = (message) => {
       console.log("control message", message);
     };
 
-    dataSocket.onopen = (event) => {
-      console.log("data open", event);
-      setWs(dataSocket);
+    data.onopen = () => {
+      setDataWs(data);
     };
 
-    dataSocket.onerror = (error) => {
-      console.log("data error", error);
+    data.onerror = () => {
+      setNotification({
+        message: "There was an error with data websocket",
+        type: "negative",
+      });
     };
 
-    dataSocket.onmessage = (message) => {
-      console.log("data message", message);
+    data.onclose = (event) => {
+      setNotification({
+        message: getWsErrorMsg(event.code),
+        type: "negative",
+      });
+    };
+
+    data.onmessage = (message) => {
       message.data.text().then((text: string) => {
         xtermRef.current?.terminal.write(text);
-        console.log(`Received: ${text} \r`);
       });
     };
   };
 
   React.useEffect(() => {
-    loadConsole();
+    openWebsockets();
   }, []);
+
+  const handleResize = () => {
+    // ensure options is not undefined. fitAddon.fit will crash otherwise
+    if (xtermRef.current && xtermRef.current.terminal.options === undefined) {
+      xtermRef.current.terminal.options = {};
+    }
+    fitAddon.fit();
+  };
+
+  const registerResize = () => {
+    if (xtermRef.current && controlWs) {
+      const terminal = xtermRef.current.terminal;
+      terminal.onResize((event) => {
+        controlWs.send(
+          JSON.stringify({
+            width: event.cols,
+            height: event.rows,
+          })
+        );
+      });
+    }
+  };
+
+  useLayoutEffect(() => {
+    registerResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [controlWs, fitAddon, xtermRef]);
 
   return (
     <>
@@ -71,35 +124,17 @@ const InstanceTerminal: FC = () => {
         <h4 className="p-panel__title">Terminal for {name}</h4>
       </div>
       <div className="p-panel__content">
+        <NotificationRow
+          notification={notification}
+          close={() => setNotification(null)}
+        />
         <Row>
           <XTerm
             ref={xtermRef}
             onData={(data) => {
-              if (ws) {
-                ws.send(data);
-                // ws.send('printenv \r', { binary: true }, () => {
-                //   setTimeout(() => ws.send('exit \r', { binary: true }), 100);
-                //   setTimeout(() => process.exit(0), 200);
-                // });
-              }
-
-              const code = data.charCodeAt(0);
-              if (code === 13 && input.length > 0) {
-                // If the user hits enter and there is input, echo it.
-                xtermRef.current?.terminal.write(
-                  "\r\nYou typed: '" + input + "'\r\n"
-                );
-                xtermRef.current?.terminal.write("$ ");
-                setInput("");
-              } else if (code < 32 || code === 127) {
-                // Disable control Keys such as arrow keys
-                return;
-              } else {
-                // Add general key press characters to the terminal
-                xtermRef.current?.terminal.write(data);
-                setInput((val) => `${val}${data}`);
-              }
+              dataWs?.send(textEncoder.encode(data));
             }}
+            addons={[fitAddon]}
           />
         </Row>
       </div>
--- static/js/helpers.tsx
+++ static/js/helpers.tsx
@@ -34,3 +34,33 @@ export const humanFileSize = (bytes: number) => {
 
   return bytes.toFixed(1) + " " + units[u];
 };
+
+export const getWsErrorMsg = (code: number) => {
+  // See https://www.rfc-editor.org/rfc/rfc6455#section-7.4.1
+  if (code == 1000)
+    return "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
+  if (code == 1001)
+    return 'An endpoint is "going away", such as a server going down or a browser having navigated away from a page.';
+  if (code == 1002)
+    return "An endpoint is terminating the connection due to a protocol error";
+  if (code == 1003)
+    return "An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).";
+  if (code == 1004)
+    return "Reserved. The specific meaning might be defined in the future.";
+  if (code == 1005) return "No status code was actually present.";
+  if (code == 1006)
+    return "The connection was closed abnormally, e.g., without sending or receiving a Close control frame";
+  if (code == 1007)
+    return "An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [https://www.rfc-editor.org/rfc/rfc3629] data within a text message).";
+  if (code == 1008)
+    return 'An endpoint is terminating the connection because it has received a message that "violates its policy". This reason is given either if there is no other sutible reason, or if there is a need to hide specific details about the policy.';
+  if (code == 1009)
+    return "An endpoint is terminating the connection because it has received a message that is too big for it to process.";
+  if (code == 1010)
+    return "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake.";
+  if (code == 1011)
+    return "A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.";
+  if (code == 1015)
+    return "The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
+  else return "Unknown reason";
+};
