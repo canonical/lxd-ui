05f9d463759f8285f62be0c7cbd7a61e8871a453
David Edler <david.edler@canonical.com>
2022-11-18 08:26:50 +0100

    feat(instance) improve error text for instance stop, which might time out

--- static/js/api/operations.tsx
+++ static/js/api/operations.tsx
@@ -6,7 +6,12 @@ export const watchOperation = (operationUrl: string, timeout: number = 10) => {
       .then(handleResponse)
       .then((data) => {
         if (data.metadata.status === "Success") {
-          resolve(data);
+          return resolve(data);
+        }
+        if (data.metadata.status === "Running") {
+          throw Error(
+            "Timeout while waiting for the operation to succeed. Watched operation continues in the background."
+          );
         } else {
           throw Error(data.metadata?.err);
         }
