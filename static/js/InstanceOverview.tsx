8ad2391a5075c6800b673e24c9f78e09375ebb36
David Edler <david.edler@canonical.com>
2022-12-13 12:30:42 +0100

    fix(instance) add notification row to overview tab

--- static/js/InstanceOverview.tsx
+++ static/js/InstanceOverview.tsx
@@ -13,6 +13,7 @@ import { Col, Row } from "@canonical/react-components";
 import StartInstanceBtn from "./buttons/instances/StartInstanceBtn";
 import StopInstanceBtn from "./buttons/instances/StopInstanceBtn";
 import EditInstanceBtn from "./buttons/instances/EditInstanceBtn";
+import NotificationRow from "./components/NotificationRow";
 
 interface Props {
   instanceName: string;
@@ -47,134 +48,139 @@ const InstanceOverview: FC<Props> = ({ instanceName }) => {
   const instanceMetrics = getInstanceMetrics(metrics, instance);
 
   return (
-    <Row>
-      <Col size={8}>
-        <table>
-          <thead>
-            <tr>
-              <th>Label</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <td>{instance.name}</td>
-            </tr>
-            <tr>
-              <th>Description</th>
-              <td>{instance.description}</td>
-            </tr>
-            <tr>
-              <th>Status</th>
-              <td>{instance.status}</td>
-            </tr>
-            <tr>
-              <th>Created at</th>
-              <td>{isoTimeToString(instance.created_at)}</td>
-            </tr>
-            <tr>
-              <th>Last used at</th>
-              <td>{isoTimeToString(instance.last_used_at)}</td>
-            </tr>
-            <tr>
-              <th>Address</th>
-              <td>
-                {instance.state?.network?.eth0?.addresses
-                  .map((item) => item.address)
-                  .join(" ")}
-              </td>
-            </tr>
-            <tr>
-              <td>Memory</td>
-              <td>
-                {instanceMetrics.memory && (
-                  <div>
-                    <Meter
-                      percentage={
-                        (100 / instanceMetrics.memory.total) *
-                        (instanceMetrics.memory.total -
-                          instanceMetrics.memory.free)
-                      }
-                      text={
-                        humanFileSize(
-                          instanceMetrics.memory.total -
-                            instanceMetrics.memory.free
-                        ) +
-                        " of " +
-                        humanFileSize(instanceMetrics.memory.total) +
-                        " memory used"
-                      }
-                    />
-                  </div>
-                )}
-              </td>
-            </tr>
-            <tr>
-              <td>Disk</td>
-              <td>
-                {instanceMetrics.disk && (
-                  <div>
-                    <Meter
-                      percentage={
-                        (100 / instanceMetrics.disk.total) *
-                        (instanceMetrics.disk.total - instanceMetrics.disk.free)
-                      }
-                      text={
-                        humanFileSize(
-                          instanceMetrics.disk.total - instanceMetrics.disk.free
-                        ) +
-                        " of " +
-                        humanFileSize(instanceMetrics.disk.total) +
-                        " disk used"
-                      }
-                    />
-                  </div>
-                )}
-              </td>
-            </tr>
-            <tr>
-              <th>Type</th>
-              <td>{instance.type}</td>
-            </tr>
-            <tr>
-              <th>Architecture</th>
-              <td>{instance.architecture}</td>
-            </tr>
-            <tr>
-              <th>Stateful</th>
-              <td>{instance.stateful ? "true" : "false"}</td>
-            </tr>
-            <tr>
-              <th>Project</th>
-              <td>{instance.project}</td>
-            </tr>
-            <tr>
-              <th>Profiles</th>
-              <td>{instance.profiles.join(", ")}</td>
-            </tr>
-            <tr>
-              <th>Location</th>
-              <td>{instance.location}</td>
-            </tr>
-            <tr>
-              <th>Ephemeral</th>
-              <td>{instance.ephemeral ? "true" : "false"}</td>
-            </tr>
-          </tbody>
-        </table>
-      </Col>
-      <Col size={4}>
-        <StartInstanceBtn instance={instance} notify={notify} />
-        <StopInstanceBtn instance={instance} notify={notify} />
-        <EditInstanceBtn instance={instance} />
-        <DeleteInstanceBtn
-          instance={instance}
-          notify={notify}
-          onFinish={() => navigate("/instances")}
-        />
-      </Col>
-    </Row>
+    <>
+      <NotificationRow notify={notify} />
+      <Row>
+        <Col size={8}>
+          <table>
+            <thead>
+              <tr>
+                <th>Label</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <td>{instance.name}</td>
+              </tr>
+              <tr>
+                <th>Description</th>
+                <td>{instance.description}</td>
+              </tr>
+              <tr>
+                <th>Status</th>
+                <td>{instance.status}</td>
+              </tr>
+              <tr>
+                <th>Created at</th>
+                <td>{isoTimeToString(instance.created_at)}</td>
+              </tr>
+              <tr>
+                <th>Last used at</th>
+                <td>{isoTimeToString(instance.last_used_at)}</td>
+              </tr>
+              <tr>
+                <th>Address</th>
+                <td>
+                  {instance.state?.network?.eth0?.addresses
+                    .map((item) => item.address)
+                    .join(" ")}
+                </td>
+              </tr>
+              <tr>
+                <td>Memory</td>
+                <td>
+                  {instanceMetrics.memory && (
+                    <div>
+                      <Meter
+                        percentage={
+                          (100 / instanceMetrics.memory.total) *
+                          (instanceMetrics.memory.total -
+                            instanceMetrics.memory.free)
+                        }
+                        text={
+                          humanFileSize(
+                            instanceMetrics.memory.total -
+                              instanceMetrics.memory.free
+                          ) +
+                          " of " +
+                          humanFileSize(instanceMetrics.memory.total) +
+                          " memory used"
+                        }
+                      />
+                    </div>
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>Disk</td>
+                <td>
+                  {instanceMetrics.disk && (
+                    <div>
+                      <Meter
+                        percentage={
+                          (100 / instanceMetrics.disk.total) *
+                          (instanceMetrics.disk.total -
+                            instanceMetrics.disk.free)
+                        }
+                        text={
+                          humanFileSize(
+                            instanceMetrics.disk.total -
+                              instanceMetrics.disk.free
+                          ) +
+                          " of " +
+                          humanFileSize(instanceMetrics.disk.total) +
+                          " disk used"
+                        }
+                      />
+                    </div>
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <th>Type</th>
+                <td>{instance.type}</td>
+              </tr>
+              <tr>
+                <th>Architecture</th>
+                <td>{instance.architecture}</td>
+              </tr>
+              <tr>
+                <th>Stateful</th>
+                <td>{instance.stateful ? "true" : "false"}</td>
+              </tr>
+              <tr>
+                <th>Project</th>
+                <td>{instance.project}</td>
+              </tr>
+              <tr>
+                <th>Profiles</th>
+                <td>{instance.profiles.join(", ")}</td>
+              </tr>
+              <tr>
+                <th>Location</th>
+                <td>{instance.location}</td>
+              </tr>
+              <tr>
+                <th>Ephemeral</th>
+                <td>{instance.ephemeral ? "true" : "false"}</td>
+              </tr>
+            </tbody>
+          </table>
+        </Col>
+        <Col size={4}>
+          <StartInstanceBtn instance={instance} notify={notify} />
+          <StopInstanceBtn instance={instance} notify={notify} />
+          <EditInstanceBtn instance={instance} />
+          <DeleteInstanceBtn
+            instance={instance}
+            notify={notify}
+            onFinish={() => navigate("/instances")}
+          />
+        </Col>
+      </Row>
+    </>
   );
 };
 
