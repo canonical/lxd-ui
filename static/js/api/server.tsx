c33b3268beefe15c24ec21e700f6cbf19ecd47e0
lorumic <michele.lorusso@canonical.com>
2022-12-14 17:17:52 +0100

    feat(resources) add resources types and api

--- static/js/api/server.tsx
+++ static/js/api/server.tsx
@@ -2,6 +2,7 @@ import { handleResponse } from "../util/helpers";
 import { LxdSettings } from "../types/server";
 import { LxdApiResponse } from "../types/apiResponse";
 import { LxdConfigPair } from "../types/config";
+import { LxdResources } from "../types/resources";
 
 export const fetchSettings = (): Promise<LxdSettings> => {
   return new Promise((resolve, reject) => {
@@ -22,6 +23,15 @@ export const updateSettings = (config: LxdConfigPair) => {
     })
       .then(handleResponse)
       .then((data) => resolve(data))
+      .catch(reject);
+  });
+};
+
+export const fetchResources = (): Promise<LxdResources> => {
+  return new Promise((resolve, reject) => {
+    fetch("/1.0/resources")
+      .then(handleResponse)
+      .then((data: LxdApiResponse<LxdResources>) => resolve(data.metadata))
       .catch(reject);
   });
 };
