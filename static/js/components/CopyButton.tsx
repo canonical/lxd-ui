39f10a7581c798d65889e5efc31ed1a90ccf2cb6
lorumic <michele.lorusso@canonical.com>
2023-01-12 16:21:48 +0100

    fix(instances) alternate display of IPv4/IPv6 information WD-1204

---
+++ static/js/components/CopyButton.tsx
@@ -0,0 +1,56 @@
+import React, { FC, useState } from "react";
+import { Tooltip, Button } from "@canonical/react-components";
+import { Position } from "@canonical/react-components/dist/components/Tooltip/Tooltip";
+
+interface Props {
+  text: string;
+  className: string;
+  tooltipPosition?: Position;
+}
+
+const CopyButton: FC<Props> = ({
+  text,
+  className,
+  tooltipPosition = "left",
+}) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className={className}>
+      <Tooltip
+        message={copied ? "Copied!" : "Copy to clipboard"}
+        position={tooltipPosition}
+      >
+        <Button
+          hasIcon
+          dense
+          aria-label={"Copy to clipboard"}
+          onClick={handleCopy}
+        >
+          <i
+            className={
+              copied ? "p-icon--task-outstanding" : "p-icon--copy-to-clipboard"
+            }
+          >
+            Copy to clipboard
+          </i>
+        </Button>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default CopyButton;
