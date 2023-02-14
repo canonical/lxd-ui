068a3e67c5e72e601e36294260b518403949166a
David Edler <david.edler@canonical.com>
2023-02-14 14:34:50 +0100

    feat(warnings) add details about warning messages WD-2018

--- static/js/api/warnings.tsx
+++ static/js/api/warnings.tsx
@@ -4,7 +4,7 @@ import { LxdApiResponse } from "types/apiResponse";
 
 export const fetchWarnings = (): Promise<LxdWarning[]> => {
   return new Promise((resolve, reject) => {
-    fetch("/1.0/warnings?recursion=2")
+    fetch("/1.0/warnings?recursion=1")
       .then(handleResponse)
       .then((data: LxdApiResponse<LxdWarning[]>) => resolve(data.metadata))
       .catch(reject);
