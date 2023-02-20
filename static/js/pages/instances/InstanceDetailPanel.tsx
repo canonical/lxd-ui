ed0668bc4fe44125e7642d0bdd72851c5cf5fc7b
lorumic <michele.lorusso@canonical.com>
2023-02-20 13:11:25 +0100

    fix(instance) apply new design to main content of instance summary panel

--- static/js/pages/instances/InstanceDetailPanel.tsx
+++ static/js/pages/instances/InstanceDetailPanel.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import OpenTerminalBtn from "./actions/OpenTerminalBtn";
 import OpenVgaBtn from "./actions/OpenVgaBtn";
 import { LxdInstance } from "types/instance";
@@ -9,6 +9,7 @@ import { isNicDevice } from "util/devices";
 import StartStopInstanceBtn from "./actions/StartStopInstanceBtn";
 import { Link } from "react-router-dom";
 import InstanceStatusIcon from "./InstanceStatusIcon";
+import { instanceCreationTypes } from "util/instanceOptions";
 
 interface Props {
   instance: LxdInstance;
@@ -31,6 +32,9 @@ const InstanceDetailPanel: FC<Props> = ({
   addStopping,
   removeLoading,
 }) => {
+  const [ip4DisplayCount, setIp4DisplayCount] = useState(5);
+  const [ip6DisplayCount, setIp6DisplayCount] = useState(5);
+
   const getIpAddresses = (family: string) => {
     return instance.state?.network?.eth0?.addresses
       .filter((item) => item.family === family)
@@ -44,8 +48,8 @@ const InstanceDetailPanel: FC<Props> = ({
         );
       });
   };
-  const ipv4Addresses = getIpAddresses("inet");
-  const ipv6Addresses = getIpAddresses("inet6");
+  const ip4Addresses = getIpAddresses("inet");
+  const ip6Addresses = getIpAddresses("inet6");
 
   return (
     <div className="p-panel p-instance-detail-panel">
@@ -94,7 +98,7 @@ const InstanceDetailPanel: FC<Props> = ({
         <table className="u-table-layout--auto">
           <tbody>
             <tr>
-              <th>Name</th>
+              <th className="u-text--muted">Name</th>
               <td>
                 <Link to={`/ui/${instance.project}/instances/${instance.name}`}>
                   {instance.name}
@@ -102,7 +106,21 @@ const InstanceDetailPanel: FC<Props> = ({
               </td>
             </tr>
             <tr>
-              <th>Status</th>
+              <th className="u-text--muted">Base image</th>
+              <td>
+                <Row>
+                  <Col
+                    size={4}
+                    className="u-truncate"
+                    title={instance.config["image.description"]}
+                  >
+                    {instance.config["image.description"] || "-"}
+                  </Col>
+                </Row>
+              </td>
+            </tr>
+            <tr>
+              <th className="u-text--muted">Status</th>
               <td>
                 <InstanceStatusIcon
                   instance={instance}
@@ -112,47 +130,79 @@ const InstanceDetailPanel: FC<Props> = ({
               </td>
             </tr>
             <tr>
-              <th>Type</th>
-              <td>{instance.type}</td>
+              <th className="u-text--muted">Description</th>
+              <td>{instance.description ? instance.description : "-"}</td>
             </tr>
             <tr>
-              <th>Architecture</th>
-              <td>{instance.architecture}</td>
-            </tr>
-            <tr>
-              <th>Image</th>
+              <th className="u-text--muted">Type</th>
               <td>
-                <Row>
-                  <Col
-                    size={4}
-                    className="u-truncate"
-                    title={instance.config["image.description"]}
-                  >
-                    {instance.config["image.description"]}
-                  </Col>
-                </Row>
+                {
+                  instanceCreationTypes.filter(
+                    (item) => item.value === instance.type
+                  )[0].label
+                }
               </td>
             </tr>
             <tr>
-              <th>Date created</th>
+              <th className="u-text--muted">IPv4</th>
+              <td>
+                {ip4Addresses && ip4Addresses.length > 0 ? (
+                  <>
+                    {ip4Addresses.slice(0, ip4DisplayCount)}
+                    {ip4DisplayCount < ip4Addresses.length && (
+                      <Button
+                        appearance="link"
+                        className="u-no-margin--bottom"
+                        small
+                        onClick={() => setIp4DisplayCount(ip4Addresses.length)}
+                      >
+                        Show all
+                      </Button>
+                    )}
+                  </>
+                ) : (
+                  "-"
+                )}
+              </td>
+            </tr>
+            <tr>
+              <th className="u-text--muted">IPv6</th>
+              <td>
+                {ip6Addresses && ip6Addresses.length > 0 ? (
+                  <>
+                    {ip6Addresses.slice(0, ip6DisplayCount)}
+                    {ip6DisplayCount < ip6Addresses.length && (
+                      <Button
+                        appearance="link"
+                        className="u-no-margin--bottom"
+                        small
+                        onClick={() => setIp6DisplayCount(ip6Addresses.length)}
+                      >
+                        Show all
+                      </Button>
+                    )}
+                  </>
+                ) : (
+                  "-"
+                )}
+              </td>
+            </tr>
+            <tr>
+              <th className="u-text--muted">Architecture</th>
+              <td>{instance.architecture}</td>
+            </tr>
+            <tr>
+              <th className="u-text--muted">PID</th>
+              <td>{instance.state?.pid ? instance.state.pid : "-"}</td>
+            </tr>
+            <tr>
+              <th className="u-text--muted">Date created</th>
               <td>{isoTimeToString(instance.created_at)}</td>
             </tr>
             <tr>
-              <th>Last used</th>
+              <th className="u-text--muted">Last used</th>
               <td>{isoTimeToString(instance.last_used_at)}</td>
             </tr>
-            {ipv4Addresses && ipv4Addresses.length > 0 && (
-              <tr>
-                <th>IPv4</th>
-                <td>{ipv4Addresses}</td>
-              </tr>
-            )}
-            {ipv6Addresses && ipv6Addresses.length > 0 && (
-              <tr>
-                <th>IPv6</th>
-                <td>{ipv6Addresses}</td>
-              </tr>
-            )}
           </tbody>
         </table>
         <h5 className="u-no-margin--bottom">Profiles</h5>
