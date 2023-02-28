6f094e83c4f7894719920c6c7068f41ebd67fccd
David Edler <david.edler@canonical.com>
2023-02-28 17:18:35 +0100

    fix(instance) adjust table and side panel after design review

--- static/js/pages/instances/InstanceDetailPanel.tsx
+++ static/js/pages/instances/InstanceDetailPanel.tsx
@@ -83,7 +83,7 @@ const InstanceDetailPanel: FC = () => {
               </Button>
             </div>
           </div>
-          <div className="p-panel__content instance-detail-panel--content">
+          <div className="p-panel__content panel-content">
             <div className="actions">
               <List
                 inline
@@ -110,204 +110,203 @@ const InstanceDetailPanel: FC = () => {
                 />
               </div>
             </div>
-            <table className="u-table-layout--auto u-no-margin--bottom">
-              <tbody>
-                <tr>
-                  <th className="u-text--muted">Name</th>
-                  <td>
-                    <Link
-                      to={`/ui/${instance.project}/instances/${instance.name}`}
-                    >
-                      {instance.name}
-                    </Link>
-                  </td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Base image</th>
-                  <td>
-                    <Row>
-                      <Col
-                        size={6}
-                        className="u-truncate"
+            <div className="content-scroll">
+              <table className="u-table-layout--auto u-no-margin--bottom">
+                <tbody>
+                  <tr>
+                    <th className="u-text--muted">Name</th>
+                    <td>
+                      <Link
+                        to={`/ui/${instance.project}/instances/${instance.name}`}
+                      >
+                        {instance.name}
+                      </Link>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="u-text--muted">Base image</th>
+                    <td>
+                      <div
+                        className="u-truncate base-image"
                         title={instance.config["image.description"]}
                       >
                         {instance.config["image.description"] || "-"}
-                      </Col>
-                    </Row>
-                  </td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Status</th>
-                  <td>
-                    <InstanceStatusIcon
-                      instance={instance}
-                      isStarting={isStarting}
-                      isStopping={isStopping}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Description</th>
-                  <td>{instance.description ? instance.description : "-"}</td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Type</th>
-                  <td>
-                    {
-                      instanceCreationTypes.filter(
-                        (item) => item.value === instance.type
-                      )[0].label
-                    }
-                  </td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">IPv4</th>
-                  <td>
-                    {ip4Addresses.length ? (
-                      <>
-                        {ip4Addresses.slice(0, ip4DisplayCount)}
-                        {ip4DisplayCount < ip4Addresses.length && (
-                          <Button
-                            appearance="link"
-                            className="u-no-margin--bottom"
-                            small
-                            onClick={() =>
-                              setIp4DisplayCount(ip4Addresses.length)
-                            }
-                          >
-                            Show all
-                          </Button>
-                        )}
-                      </>
-                    ) : (
-                      "-"
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">IPv6</th>
-                  <td>
-                    {ip6Addresses.length ? (
-                      <>
-                        {ip6Addresses.slice(0, ip6DisplayCount)}
-                        {ip6DisplayCount < ip6Addresses.length && (
-                          <Button
-                            appearance="link"
-                            className="u-no-margin--bottom"
-                            small
-                            onClick={() =>
-                              setIp6DisplayCount(ip6Addresses.length)
-                            }
-                          >
-                            Show all
-                          </Button>
-                        )}
-                      </>
-                    ) : (
-                      "-"
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Architecture</th>
-                  <td>{instance.architecture}</td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Created</th>
-                  <td>{isoTimeToString(instance.created_at)}</td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Last used</th>
-                  <td>{isoTimeToString(instance.last_used_at)}</td>
-                </tr>
-              </tbody>
-            </table>
-            <Row className="no-grid-gap">
-              <Col size={6}>
-                <h5 className="p-muted-heading">Profiles</h5>
-              </Col>
-              <Col
-                size={6}
-                title="View configurations"
-                className="p-action-link p-text--small u-align--right u-no-margin--bottom u-truncate"
-              >
-                <Link
-                  // TODO: fix this link to point to the configurations tab
-                  to={`/ui/${instance.project}/instances/${instance.name}`}
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="u-text--muted">Status</th>
+                    <td>
+                      <InstanceStatusIcon
+                        instance={instance}
+                        isStarting={isStarting}
+                        isStopping={isStopping}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="u-text--muted">Description</th>
+                    <td>{instance.description ? instance.description : "-"}</td>
+                  </tr>
+                  <tr>
+                    <th className="u-text--muted">Type</th>
+                    <td>
+                      {
+                        instanceCreationTypes.filter(
+                          (item) => item.value === instance.type
+                        )[0].label
+                      }
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="u-text--muted">IPv4</th>
+                    <td>
+                      {ip4Addresses.length ? (
+                        <>
+                          {ip4Addresses.slice(0, ip4DisplayCount)}
+                          {ip4DisplayCount < ip4Addresses.length && (
+                            <Button
+                              appearance="link"
+                              className="u-no-margin--bottom"
+                              small
+                              onClick={() =>
+                                setIp4DisplayCount(ip4Addresses.length)
+                              }
+                            >
+                              Show all
+                            </Button>
+                          )}
+                        </>
+                      ) : (
+                        "-"
+                      )}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="u-text--muted">IPv6</th>
+                    <td>
+                      {ip6Addresses.length ? (
+                        <>
+                          {ip6Addresses.slice(0, ip6DisplayCount)}
+                          {ip6DisplayCount < ip6Addresses.length && (
+                            <Button
+                              appearance="link"
+                              className="u-no-margin--bottom"
+                              small
+                              onClick={() =>
+                                setIp6DisplayCount(ip6Addresses.length)
+                              }
+                            >
+                              Show all
+                            </Button>
+                          )}
+                        </>
+                      ) : (
+                        "-"
+                      )}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="u-text--muted">Architecture</th>
+                    <td>{instance.architecture}</td>
+                  </tr>
+                  <tr>
+                    <th className="u-text--muted">Created</th>
+                    <td>{isoTimeToString(instance.created_at)}</td>
+                  </tr>
+                  <tr>
+                    <th className="u-text--muted">Last used</th>
+                    <td>{isoTimeToString(instance.last_used_at)}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <Row className="no-grid-gap">
+                <Col size={6}>
+                  <h5 className="p-muted-heading">Profiles</h5>
+                </Col>
+                <Col
+                  size={6}
+                  title="View configurations"
+                  className="p-action-link p-text--small u-align--right u-no-margin--bottom u-truncate"
                 >
-                  View configurations
-                </Link>
-              </Col>
-            </Row>
-            <List
-              className="u-no-margin--bottom"
-              items={instance.profiles.map((name) => (
-                <Link
-                  key={name}
-                  to={`/ui/${instance.project}/profiles/${name}`}
-                >
-                  {name}
-                </Link>
-              ))}
-            />
-            <hr className="u-spaced-hr p-section-separator" />
-            <h5 className="p-muted-heading">Networks</h5>
-            <List
-              className="u-no-margin--bottom"
-              items={Object.values(instance.expanded_devices)
-                .filter(isNicDevice)
-                .map((item) => (
-                  // TODO: fix this link to point to the network detail page
                   <Link
-                    key={item.network}
-                    to={`/ui/${instance.project}/networks`}
+                    // TODO: fix this link to point to the configurations tab
+                    to={`/ui/${instance.project}/instances/${instance.name}`}
                   >
-                    {item.network}
+                    View configurations
+                  </Link>
+                </Col>
+              </Row>
+              <List
+                className="u-no-margin--bottom"
+                items={instance.profiles.map((name) => (
+                  <Link
+                    key={name}
+                    to={`/ui/${instance.project}/profiles/${name}`}
+                  >
+                    {name}
                   </Link>
                 ))}
-            />
-            <hr className="u-spaced-hr p-section-separator" />
-            <Row className="no-grid-gap">
-              <Col size={8}>
-                <h5 className="p-muted-heading">Recent Snapshots</h5>
-              </Col>
-              <Col
-                size={4}
-                title={manageSnapshotLabel}
-                className="p-action-link p-text--small u-align--right u-no-margin--bottom u-truncate"
-              >
-                <Link
-                  to={`/ui/${instance.project}/instances/${instance.name}/snapshots`}
+              />
+              <hr className="u-spaced-hr p-section-separator" />
+              <h5 className="p-muted-heading">Networks</h5>
+              <List
+                className="u-no-margin--bottom"
+                items={Object.values(instance.expanded_devices)
+                  .filter(isNicDevice)
+                  .map((item) => (
+                    // TODO: fix this link to point to the network detail page
+                    <Link
+                      key={item.network}
+                      to={`/ui/${instance.project}/networks`}
+                    >
+                      {item.network}
+                    </Link>
+                  ))}
+              />
+              <hr className="u-spaced-hr p-section-separator" />
+              <Row className="no-grid-gap">
+                <Col size={8}>
+                  <h5 className="p-muted-heading">Recent Snapshots</h5>
+                </Col>
+                <Col
+                  size={4}
+                  title={manageSnapshotLabel}
+                  className="p-action-link p-text--small u-align--right u-no-margin--bottom u-truncate"
                 >
-                  {manageSnapshotLabel}
-                </Link>
-              </Col>
-            </Row>
-            <List
-              className="u-no-margin--bottom"
-              items={
-                instance.snapshots
-                  ?.reverse()
-                  .slice(0, RECENT_SNAPSHOT_LIMIT)
-                  .map((snapshot) => (
-                    <Row key={snapshot.name} className="no-grid-gap">
-                      <Col
-                        size={4}
-                        className="u-truncate"
-                        title={snapshot.name}
-                      >
-                        {snapshot.name}
-                      </Col>
-                      <Col
-                        size={8}
-                        className="p-snapshot-creation u-align--right u-text--muted u-no-margin--bottom"
-                      >
-                        <i>{isoTimeToString(snapshot.created_at)}</i>
-                      </Col>
-                    </Row>
-                  )) ?? ["None"]
-              }
-            />
+                  <Link
+                    to={`/ui/${instance.project}/instances/${instance.name}/snapshots`}
+                  >
+                    {manageSnapshotLabel}
+                  </Link>
+                </Col>
+              </Row>
+              <List
+                className="u-no-margin--bottom"
+                items={
+                  instance.snapshots
+                    ?.reverse()
+                    .slice(0, RECENT_SNAPSHOT_LIMIT)
+                    .map((snapshot) => (
+                      <Row key={snapshot.name} className="no-grid-gap">
+                        <Col
+                          size={4}
+                          className="u-truncate"
+                          title={snapshot.name}
+                        >
+                          {snapshot.name}
+                        </Col>
+                        <Col
+                          size={8}
+                          className="p-snapshot-creation u-align--right u-text--muted u-no-margin--bottom"
+                        >
+                          <i>{isoTimeToString(snapshot.created_at)}</i>
+                        </Col>
+                      </Row>
+                    )) ?? ["None"]
+                }
+              />
+            </div>
           </div>
         </div>
       )}
