049b8afbaba9957a440bdb3f9220780f12af4457
David Edler <david.edler@canonical.com>
2023-03-07 16:25:49 +0100

    fix(snapshots) resolve merge

--- static/js/pages/instances/actions/snapshots/CreateSnapshotBtn.tsx
+++ static/js/pages/instances/actions/snapshots/CreateSnapshotBtn.tsx
@@ -2,17 +2,15 @@ import React, { FC } from "react";
 import { Button } from "@canonical/react-components";
 
 interface Props {
-  openModal: () => void;
+  openSnapshotForm: () => void;
 }
 
-const CreateSnapshotBtn: FC<Props> = ({ openModal }) => {
+const CreateSnapshotBtn: FC<Props> = ({ openSnapshotForm }) => {
   return (
-    <>
-      <Button className="u-no-margin--bottom" hasIcon onClick={openModal}>
-        <i className="p-icon--plus" />
-        <span>Create snapshot</span>
-      </Button>
-    </>
+    <Button className="u-no-margin--bottom" hasIcon onClick={openSnapshotForm}>
+      <i className="p-icon--plus" />
+      <span>Create snapshot</span>
+    </Button>
   );
 };
 
