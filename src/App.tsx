9e2e3b603ce67dc18bb1b3289a8ff570cf19a79b
David Edler <david.edler@canonical.com>
2023-03-14 18:16:46 +0100

    fix(build) optimize initial bundle size, remove fonts, remove webpack WD-1229

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,123 @@
-import React, { FC } from "react";
+import React, { FC, lazy, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import ClusterList from "pages/cluster/ClusterList";
-import ImageList from "pages/images/ImageList";
-import InstanceList from "pages/instances/InstanceList";
-import ProfileList from "pages/profiles/ProfileList";
-import NetworkList from "pages/networks/NetworkList";
-import NoMatch from "components/NoMatch";
-import ProjectList from "pages/projects/ProjectList";
-import WarningList from "pages/warnings/WarningList";
-import Settings from "pages/settings/Settings";
-import InstanceDetail from "pages/instances/InstanceDetail";
-import StorageList from "pages/storages/StorageList";
-import ProfileDetail from "pages/profiles/ProfileDetail";
-import CertificateGenerate from "pages/certificates/CertificateGenerate";
-import CertificateMain from "pages/certificates/CertificateMain";
-import ProtectedRoute from "components/ProtectedRoute";
-import StorageDetail from "pages/storages/StorageDetail";
-import NetworkMap from "pages/networks/NetworkMap";
-import CreateInstanceForm from "pages/instances/CreateInstanceForm";
-import CreateProfileForm from "pages/profiles/CreateProfileForm";
+import Loader from "components/Loader";
+
+const ClusterList = lazy(() => import("pages/cluster/ClusterList"));
+const ImageList = lazy(() => import("pages/images/ImageList"));
+const InstanceList = lazy(() => import("pages/instances/InstanceList"));
+const ProfileList = lazy(() => import("pages/profiles/ProfileList"));
+const NetworkList = lazy(() => import("./pages/networks/NetworkList"));
+const NoMatch = lazy(() => import("components/NoMatch"));
+const ProjectList = lazy(() => import("pages/projects/ProjectList"));
+const WarningList = lazy(() => import("pages/warnings/WarningList"));
+const Settings = lazy(() => import("pages/settings/Settings"));
+const InstanceDetail = lazy(() => import("pages/instances/InstanceDetail"));
+const StorageList = lazy(() => import("pages/storages/StorageList"));
+const ProfileDetail = lazy(() => import("pages/profiles/ProfileDetail"));
+const CertificateGenerate = lazy(
+  () => import("pages/certificates/CertificateGenerate")
+);
+const CertificateMain = lazy(
+  () => import("pages/certificates/CertificateMain")
+);
+const ProtectedRoute = lazy(() => import("components/ProtectedRoute"));
+const StorageDetail = lazy(() => import("pages/storages/StorageDetail"));
+const NetworkMap = lazy(() => import("pages/networks/NetworkMap"));
+const CreateInstanceForm = lazy(
+  () => import("pages/instances/CreateInstanceForm")
+);
+const CreateProfileForm = lazy(
+  () => import("pages/profiles/CreateProfileForm")
+);
 
 const App: FC = () => {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Navigate to="/ui/default/instances" replace={true} />}
-      />
-      <Route
-        path="/ui"
-        element={<Navigate to="/ui/default/instances" replace={true} />}
-      />
-      <Route
-        path="/ui/:project/instances"
-        element={<ProtectedRoute outlet={<InstanceList />} />}
-      />
-      <Route
-        path="/ui/:project/instances/create"
-        element={<ProtectedRoute outlet={<CreateInstanceForm />} />}
-      />
-      <Route
-        path="/ui/:project/instances/detail/:name"
-        element={<ProtectedRoute outlet={<InstanceDetail />} />}
-      />
-      <Route
-        path="/ui/:project/instances/detail/:name/:activeTab"
-        element={<ProtectedRoute outlet={<InstanceDetail />} />}
-      />
-      <Route
-        path="/ui/images"
-        element={<ProtectedRoute outlet={<ImageList />} />}
-      />
-      <Route
-        path="/ui/:project/profiles"
-        element={<ProtectedRoute outlet={<ProfileList />} />}
-      />
-      <Route
-        path="/ui/:project/profiles/create"
-        element={<ProtectedRoute outlet={<CreateProfileForm />} />}
-      />
-      <Route
-        path="/ui/:project/profiles/detail/:name"
-        element={<ProtectedRoute outlet={<ProfileDetail />} />}
-      />
-      <Route
-        path="/ui/:project/profiles/detail/:name/:activeTab"
-        element={<ProtectedRoute outlet={<ProfileDetail />} />}
-      />
-      <Route
-        path="/ui/:project/networks"
-        element={<ProtectedRoute outlet={<NetworkList />} />}
-      />
-      <Route
-        path="/ui/:project/networks/map"
-        element={<ProtectedRoute outlet={<NetworkMap />} />}
-      />
-      <Route
-        path="/ui/projects"
-        element={<ProtectedRoute outlet={<ProjectList />} />}
-      />
-      <Route
-        path="/ui/:project/storages"
-        element={<ProtectedRoute outlet={<StorageList />} />}
-      />
-      <Route
-        path="/ui/:project/storages/:name"
-        element={<ProtectedRoute outlet={<StorageDetail />} />}
-      />
-      <Route
-        path="/ui/cluster"
-        element={<ProtectedRoute outlet={<ClusterList />} />}
-      />
-      <Route
-        path="/ui/warnings"
-        element={<ProtectedRoute outlet={<WarningList />} />}
-      />
-      <Route
-        path="/ui/settings"
-        element={<ProtectedRoute outlet={<Settings />} />}
-      />
-      <Route path="/ui/certificates" element={<CertificateMain />} />
-      <Route
-        path="/ui/certificates/generate"
-        element={<CertificateGenerate />}
-      />
-      <Route path="*" element={<NoMatch />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to="/ui/default/instances" replace={true} />}
+        />
+        <Route
+          path="/ui"
+          element={<Navigate to="/ui/default/instances" replace={true} />}
+        />
+        <Route
+          path="/ui/:project/instances"
+          element={<ProtectedRoute outlet={<InstanceList />} />}
+        />
+        <Route
+          path="/ui/:project/instances/create"
+          element={<ProtectedRoute outlet={<CreateInstanceForm />} />}
+        />
+        <Route
+          path="/ui/:project/instances/detail/:name"
+          element={<ProtectedRoute outlet={<InstanceDetail />} />}
+        />
+        <Route
+          path="/ui/:project/instances/detail/:name/:activeTab"
+          element={<ProtectedRoute outlet={<InstanceDetail />} />}
+        />
+        <Route
+          path="/ui/images"
+          element={<ProtectedRoute outlet={<ImageList />} />}
+        />
+        <Route
+          path="/ui/:project/profiles"
+          element={<ProtectedRoute outlet={<ProfileList />} />}
+        />
+        <Route
+          path="/ui/:project/profiles/create"
+          element={<ProtectedRoute outlet={<CreateProfileForm />} />}
+        />
+        <Route
+          path="/ui/:project/profiles/detail/:name"
+          element={<ProtectedRoute outlet={<ProfileDetail />} />}
+        />
+        <Route
+          path="/ui/:project/profiles/detail/:name/:activeTab"
+          element={<ProtectedRoute outlet={<ProfileDetail />} />}
+        />
+        <Route
+          path="/ui/:project/networks"
+          element={<ProtectedRoute outlet={<NetworkList />} />}
+        />
+        <Route
+          path="/ui/:project/networks/map"
+          element={<ProtectedRoute outlet={<NetworkMap />} />}
+        />
+        <Route
+          path="/ui/projects"
+          element={<ProtectedRoute outlet={<ProjectList />} />}
+        />
+        <Route
+          path="/ui/:project/storages"
+          element={<ProtectedRoute outlet={<StorageList />} />}
+        />
+        <Route
+          path="/ui/:project/storages/:name"
+          element={<ProtectedRoute outlet={<StorageDetail />} />}
+        />
+        <Route
+          path="/ui/cluster"
+          element={<ProtectedRoute outlet={<ClusterList />} />}
+        />
+        <Route
+          path="/ui/warnings"
+          element={<ProtectedRoute outlet={<WarningList />} />}
+        />
+        <Route
+          path="/ui/settings"
+          element={<ProtectedRoute outlet={<Settings />} />}
+        />
+        <Route path="/ui/certificates" element={<CertificateMain />} />
+        <Route
+          path="/ui/certificates/generate"
+          element={<CertificateGenerate />}
+        />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Suspense>
   );
 };
 
