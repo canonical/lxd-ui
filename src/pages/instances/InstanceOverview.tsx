a992f9ab3f42edee6e954c5fe34b33d1c6fc75a4
lorumic <michele.lorusso@canonical.com>
2023-03-23 18:30:28 +0100

    fix(overview) overhaul instance detail tab: overview WD-2551

--- src/pages/instances/InstanceOverview.tsx
+++ src/pages/instances/InstanceOverview.tsx
@@ -1,25 +1,32 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "util/queryKeys";
 import { fetchMetrics } from "api/metrics";
 import { humanFileSize, isoTimeToString } from "util/helpers";
 import { getInstanceMetrics } from "util/metricSelectors";
 import Meter from "components/Meter";
-import DeleteInstanceBtn from "./actions/DeleteInstanceBtn";
-import { createPortal } from "react-dom";
-import { List } from "@canonical/react-components";
+import { Col, MainTable, Row } from "@canonical/react-components";
 import Loader from "components/Loader";
 import { LxdInstance } from "types/instance";
 import { Link } from "react-router-dom";
-import { useNotify } from "context/notify";
+import { instanceCreationTypes } from "util/instanceOptions";
+import { fetchNetworks } from "api/networks";
+import { isNicDevice } from "util/devices";
+import { fetchProfiles } from "api/profiles";
+import { Notification } from "types/notification";
+import NotificationRowLegacy from "components/NotificationRowLegacy";
+import { failure } from "context/notify";
+import useEventListener from "@use-it/event-listener";
+import { updateMaxHeight } from "util/updateMaxHeight";
 
 interface Props {
-  controlTarget?: HTMLSpanElement | null;
   instance: LxdInstance;
 }
 
-const InstanceOverview: FC<Props> = ({ controlTarget, instance }) => {
-  const notify = useNotify();
+const InstanceOverview: FC<Props> = ({ instance }) => {
+  const [inTabNotification, setInTabNotification] =
+    useState<Notification | null>(null);
+
   const {
     data: metrics = [],
     error: metricError,
@@ -31,162 +38,282 @@ const InstanceOverview: FC<Props> = ({ controlTarget, instance }) => {
   });
 
   if (metricError) {
-    notify.failure("Could not load metrics.", metricError);
+    setInTabNotification(failure("Could not load metrics.", metricError));
   }
+
+  const instanceMetrics = getInstanceMetrics(metrics, instance);
+
+  const {
+    data: networks = [],
+    error: networkError,
+    isLoading: isNetworkLoading,
+  } = useQuery({
+    queryKey: [queryKeys.networks, instance.project],
+    queryFn: () => fetchNetworks(instance.project),
+  });
+
+  if (networkError) {
+    setInTabNotification(failure("Could not load networks.", networkError));
+  }
+
+  const instanceNetworks = Object.values(instance.expanded_devices)
+    .filter(isNicDevice)
+    .map((network) => network.network);
+
+  const networksHeaders = [
+    { content: "Name", sortKey: "name" },
+    { content: "Type", sortKey: "type" },
+    { content: "Managed", sortKey: "managed" },
+    { content: "IPV4", className: "u-align--right" },
+    { content: "IPV6" },
+  ];
+
+  const networksRows = networks
+    .filter((network) => instanceNetworks.includes(network.name))
+    .map((network) => {
+      return {
+        columns: [
+          {
+            content: (
+              // TODO: fix this link to point to the network detail page
+              <Link
+                to={`/ui/${instance.project}/networks`}
+                title={network.name}
+              >
+                {network.name}
+              </Link>
+            ),
+            role: "rowheader",
+            "aria-label": "Name",
+          },
+          {
+            content: network.type,
+            role: "rowheader",
+            "aria-label": "Type",
+          },
+          {
+            content: network.managed ? "Yes" : "No",
+            role: "rowheader",
+            "aria-label": "Managed",
+          },
+          {
+            content: network.config["ipv4.address"],
+            className: "u-align--right",
+            role: "rowheader",
+            title: network.config["ipv4.address"],
+            "aria-label": "IPV4",
+          },
+          {
+            content: network.config["ipv6.address"],
+            role: "rowheader",
+            title: network.config["ipv6.address"],
+            "aria-label": "IPV6",
+          },
+        ],
+        sortData: {
+          name: network.name,
+          type: network.type,
+          managed: network.managed,
+        },
+      };
+    });
+
+  const {
+    data: profiles = [],
+    error: profileError,
+    isLoading: isProfileLoading,
+  } = useQuery({
+    queryKey: [queryKeys.profiles, instance.project],
+    queryFn: () => fetchProfiles(instance.project),
+  });
+
+  if (profileError) {
+    setInTabNotification(failure("Could not load profiles.", profileError));
+  }
+
+  const profileHeaders = [
+    { content: "Name", sortKey: "name" },
+    { content: "Description", sortKey: "description" },
+  ];
+
+  const profileRows = profiles
+    .filter((profile) => instance.profiles.includes(profile.name))
+    .map((profile) => {
+      return {
+        columns: [
+          {
+            content: (
+              <Link
+                to={`/ui/${instance.project}/profiles/detail/${profile.name}`}
+                title={profile.name}
+              >
+                {profile.name}
+              </Link>
+            ),
+            role: "rowheader",
+            "aria-label": "Name",
+          },
+          {
+            content: profile.description,
+            role: "rowheader",
+            title: profile.description,
+            "aria-label": "Description",
+          },
+        ],
+        sortData: {
+          name: profile.name,
+          description: profile.description,
+        },
+      };
+    });
+
+  const updateContentHeight = () => {
+    updateMaxHeight("instance-overview-tab");
+  };
+  useEffect(updateContentHeight, [inTabNotification]);
+  useEventListener("resize", updateContentHeight);
 
   if (isMetricLoading) {
     return <Loader text="Loading metrics..." />;
   }
 
-  const instanceMetrics = getInstanceMetrics(metrics, instance);
+  if (isNetworkLoading) {
+    return <Loader text="Loading networks..." />;
+  }
 
-  const getIpAddresses = (family: string) => {
-    return instance.state?.network?.eth0?.addresses
-      .filter((item) => item.family === family)
-      .map((item) => {
-        return (
-          <div key={item.address} className="u-truncate" title={item.address}>
-            {item.address}
-          </div>
-        );
-      });
-  };
-  const ipv4Addresses = getIpAddresses("inet");
-  const ipv6Addresses = getIpAddresses("inet6");
+  if (isProfileLoading) {
+    return <Loader text="Loading profiles..." />;
+  }
 
   return (
-    <>
-      {controlTarget &&
-        createPortal(<DeleteInstanceBtn instance={instance} />, controlTarget)}
-      <table>
-        <tbody>
-          <tr>
-            <th className="u-text--muted">Name</th>
-            <td>{instance.name}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Description</th>
-            <td>{instance.description ? instance.description : "-"}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Created at</th>
-            <td>{isoTimeToString(instance.created_at)}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Last used at</th>
-            <td>{isoTimeToString(instance.last_used_at)}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">IPv4</th>
-            <td>
-              {ipv4Addresses && ipv4Addresses.length > 0 ? ipv4Addresses : "-"}
-            </td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">IPv6</th>
-            <td>
-              {ipv6Addresses && ipv6Addresses.length > 0 ? ipv6Addresses : "-"}
-            </td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Memory</th>
-            <td>
-              {instanceMetrics.memory ? (
-                <div>
-                  <Meter
-                    percentage={
-                      (100 / instanceMetrics.memory.total) *
-                      (instanceMetrics.memory.total -
-                        instanceMetrics.memory.free)
-                    }
-                    text={
-                      humanFileSize(
-                        instanceMetrics.memory.total -
-                          instanceMetrics.memory.free
-                      ) +
-                      " of " +
-                      humanFileSize(instanceMetrics.memory.total) +
-                      " memory used"
-                    }
-                  />
-                </div>
-              ) : (
-                "-"
-              )}
-            </td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Disk</th>
-            <td>
-              {instanceMetrics.disk ? (
-                <div>
-                  <Meter
-                    percentage={
-                      (100 / instanceMetrics.disk.total) *
-                      (instanceMetrics.disk.total - instanceMetrics.disk.free)
-                    }
-                    text={
-                      humanFileSize(
-                        instanceMetrics.disk.total - instanceMetrics.disk.free
-                      ) +
-                      " of " +
-                      humanFileSize(instanceMetrics.disk.total) +
-                      " disk used"
-                    }
-                  />
-                </div>
-              ) : (
-                "-"
-              )}
-            </td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Type</th>
-            <td>{instance.type}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Architecture</th>
-            <td>{instance.architecture}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Image</th>
-            <td>{instance.config["image.description"]}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Stateful</th>
-            <td>{instance.stateful ? "true" : "false"}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Project</th>
-            <td>{instance.project}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Profiles</th>
-            <td>
-              <List
-                className="u-no-margin--bottom"
-                items={instance.profiles.map((name) => (
-                  <Link
-                    key={name}
-                    to={`/ui/${instance.project}/profiles/detail/${name}`}
-                  >
-                    {name}
-                  </Link>
-                ))}
-              />
-            </td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Location</th>
-            <td>{instance.location}</td>
-          </tr>
-          <tr>
-            <th className="u-text--muted">Ephemeral</th>
-            <td>{instance.ephemeral ? "true" : "false"}</td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+    <div className="instance-overview-tab">
+      <NotificationRowLegacy
+        notification={inTabNotification}
+        onDismiss={() => setInTabNotification(null)}
+      />
+      <Row className="general">
+        <Col size={3}>
+          <h2 className="p-heading--4">General</h2>
+        </Col>
+        <Col size={7}>
+          <table>
+            <tbody>
+              <tr>
+                <th className="p-muted-heading">Base image</th>
+                <td>{instance.config["image.description"] ?? "-"}</td>
+              </tr>
+              <tr>
+                <th className="p-muted-heading">Description</th>
+                <td>{instance.description ? instance.description : "-"}</td>
+              </tr>
+              <tr>
+                <th className="p-muted-heading">Type</th>
+                <td>
+                  {
+                    instanceCreationTypes.filter(
+                      (item) => item.value === instance.type
+                    )[0].label
+                  }
+                </td>
+              </tr>
+              <tr>
+                <th className="p-muted-heading">Architecture</th>
+                <td>{instance.architecture}</td>
+              </tr>
+              <tr>
+                <th className="p-muted-heading">Date created</th>
+                <td>{isoTimeToString(instance.created_at)}</td>
+              </tr>
+              <tr>
+                <th className="p-muted-heading">Last used</th>
+                <td>{isoTimeToString(instance.last_used_at)}</td>
+              </tr>
+            </tbody>
+          </table>
+        </Col>
+      </Row>
+      <Row className="usage">
+        <Col size={3}>
+          <h2 className="p-heading--4">Usage</h2>
+        </Col>
+        <Col size={7}>
+          <table>
+            <tbody>
+              <tr className="metric-row">
+                <th className="p-muted-heading">Memory</th>
+                <td>
+                  {instanceMetrics.memory ? (
+                    <div>
+                      <Meter
+                        percentage={
+                          (100 / instanceMetrics.memory.total) *
+                          (instanceMetrics.memory.total -
+                            instanceMetrics.memory.free)
+                        }
+                        text={
+                          humanFileSize(
+                            instanceMetrics.memory.total -
+                              instanceMetrics.memory.free
+                          ) +
+                          " of " +
+                          humanFileSize(instanceMetrics.memory.total) +
+                          " memory used"
+                        }
+                      />
+                    </div>
+                  ) : (
+                    "-"
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <th className="p-muted-heading">Disk</th>
+                <td>
+                  {instanceMetrics.disk ? (
+                    <div>
+                      <Meter
+                        percentage={
+                          (100 / instanceMetrics.disk.total) *
+                          (instanceMetrics.disk.total -
+                            instanceMetrics.disk.free)
+                        }
+                        text={
+                          humanFileSize(
+                            instanceMetrics.disk.total -
+                              instanceMetrics.disk.free
+                          ) +
+                          " of " +
+                          humanFileSize(instanceMetrics.disk.total) +
+                          " disk used"
+                        }
+                      />
+                    </div>
+                  ) : (
+                    "-"
+                  )}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Col>
+      </Row>
+      <Row className="networks">
+        <Col size={3}>
+          <h2 className="p-heading--4">Networks</h2>
+        </Col>
+        <Col size={7}>
+          <MainTable headers={networksHeaders} rows={networksRows} sortable />
+        </Col>
+      </Row>
+      <Row className="profiles">
+        <Col size={3}>
+          <h2 className="p-heading--4">Profiles</h2>
+        </Col>
+        <Col size={7}>
+          <MainTable headers={profileHeaders} rows={profileRows} sortable />
+        </Col>
+      </Row>
+    </div>
   );
 };
 
