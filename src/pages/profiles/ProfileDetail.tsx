703da272f48f268771adfdaf7b21d9b741001007
lorumic <michele.lorusso@canonical.com>
2023-04-09 11:19:17 +0200

    fix(infra) extract profile header, fix name of instance header

--- src/pages/profiles/ProfileDetail.tsx
+++ src/pages/profiles/ProfileDetail.tsx
@@ -1,16 +1,15 @@
-import React, { FC, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import React, { FC } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { fetchProfile } from "api/profiles";
 import NotificationRow from "components/NotificationRow";
 import { queryKeys } from "util/queryKeys";
 import { useNotify } from "context/notify";
-import { Row, Tabs, Tooltip } from "@canonical/react-components";
+import { Row, Tabs } from "@canonical/react-components";
 import Loader from "components/Loader";
 import EditProfileForm from "pages/profiles/EditProfileForm";
 import ProfileDetailOverview from "pages/profiles/ProfileDetailOverview";
-import ProfileRename from "pages/profiles/forms/ProfileRename";
-import DeleteProfileBtn from "./actions/DeleteProfileBtn";
+import ProfileDetailHeader from "./ProfileDetailHeader";
 
 const TABS: string[] = ["Overview", "Configuration"];
 
@@ -26,7 +25,6 @@ const ProfileDetail: FC = () => {
     project: string;
     activeTab?: string;
   }>();
-  const [isRename, setRename] = useState(false);
 
   if (!name) {
     return <>Missing name</>;
@@ -57,60 +55,10 @@ const ProfileDetail: FC = () => {
     }
   };
 
-  const canRename = profile?.name !== "default";
-
   return (
     <main className="l-main">
       <div className="p-panel profile-detail-page">
-        <div className="p-panel__header">
-          <h1 className="u-off-screen">{name}</h1>
-          {profile ? (
-            <div className="p-panel__title">
-              <nav
-                key="breadcrumbs"
-                className="p-breadcrumbs"
-                aria-label="Breadcrumbs"
-              >
-                <ol className="p-breadcrumbs__items">
-                  <li className="p-breadcrumbs__item">
-                    <Link to={`/ui/${project}/profiles`}>Profiles</Link>
-                  </li>
-                  {isRename ? (
-                    <li className="p-breadcrumbs__item profile-rename">
-                      <ProfileRename
-                        profile={profile}
-                        project={project}
-                        closeForm={() => setRename(false)}
-                      />
-                    </li>
-                  ) : (
-                    <li
-                      className="p-breadcrumbs__item profile-name u-truncate"
-                      onClick={() => canRename && setRename(true)}
-                      title={name}
-                    >
-                      <Tooltip
-                        message={
-                          !canRename && "Cannot rename the default profile"
-                        }
-                        position="btm-left"
-                      >
-                        {name}
-                      </Tooltip>
-                    </li>
-                  )}
-                </ol>
-              </nav>
-            </div>
-          ) : (
-            <h4 className="p-panel__title">{name}</h4>
-          )}
-          <div className="p-panel__controls">
-            {profile && !isRename && (
-              <DeleteProfileBtn profile={profile} project={project} />
-            )}
-          </div>
-        </div>
+        <ProfileDetailHeader name={name} profile={profile} project={project} />
         <div className="p-panel__content">
           <NotificationRow />
           {isLoading && <Loader text="Loading profile details..." />}
