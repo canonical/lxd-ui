1f07d436127820045c41e61b535c37406f36c812
lorumic <michele.lorusso@canonical.com>
2023-04-19 21:56:45 +0200

    fix(snapshot-restore) force stateful to false for stateless snapshots

--- src/util/snapshots.tsx
+++ src/util/snapshots.tsx
@@ -10,13 +10,12 @@ export interface SnapshotFormValues {
   expirationTime: string | null;
 }
 
-export const getExpiresAt = (
-  expirationDate: string,
-  expirationTime: string | null
-) => {
-  expirationTime = expirationTime ?? "00:00";
-  return `${expirationDate}T${expirationTime}`;
+export const isInstanceStateful = (instance: LxdInstance) => {
+  return Boolean(instance.config["migration.stateful"]);
 };
+
+export const getExpiresAt = (expirationDate: string, expirationTime: string) =>
+  `${expirationDate}T${expirationTime}`;
 
 export const testDuplicateName = (
   instance: LxdInstance,
