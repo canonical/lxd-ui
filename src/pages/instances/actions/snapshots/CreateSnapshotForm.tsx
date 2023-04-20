99c27ad6dc8cb00a5d9804322a8e26d9be8b351a
lorumic <michele.lorusso@canonical.com>
2023-04-20 09:20:42 +0200

    fix(snapshot-schema) generalise schema and move it to the shared util

--- src/pages/instances/actions/snapshots/CreateSnapshotForm.tsx
+++ src/pages/instances/actions/snapshots/CreateSnapshotForm.tsx
@@ -10,13 +10,10 @@ import ItemName from "components/ItemName";
 import {
   SnapshotFormValues,
   getExpiresAt,
+  getSnapshotSchema,
   isInstanceStateful,
-  testDuplicateName,
-  testForbiddenChars,
-  testFutureDate,
 } from "util/snapshots";
 import SnapshotForm from "./SnapshotForm";
-import * as Yup from "yup";
 
 interface Props {
   instance: LxdInstance;
@@ -29,36 +26,7 @@ const CreateSnapshotForm: FC<Props> = ({ instance, close, onSuccess }) => {
   const queryClient = useQueryClient();
   const controllerState = useState<AbortController | null>(null);
 
-  const SnapshotSchema: unknown = Yup.object().shape({
-    name: Yup.string()
-      .required("This field is required")
-      .test(...testDuplicateName(instance, controllerState))
-      .test(...testForbiddenChars()),
-    expirationDate: Yup.string()
-      .nullable()
-      .optional()
-      .test("valid", "Invalid date format", (value) => {
-        if (!value) {
-          return !formik.values.expirationTime;
-        }
-        return new Date(value).toString() !== "Invalid Date";
-      })
-      .test(...testFutureDate()),
-    expirationTime: Yup.string()
-      .nullable()
-      .optional()
-      .test("valid", "Invalid time format", (value) => {
-        if (!value) {
-          return !formik.values.expirationDate;
-        }
-        const [hours, minutes] = value.split(":");
-        const date = new Date();
-        date.setHours(+hours);
-        date.setMinutes(+minutes);
-        return date.toString() !== "Invalid Date";
-      }),
-    stateful: Yup.boolean(),
-  });
+  const SnapshotSchema = getSnapshotSchema(instance, controllerState);
 
   const formik = useFormik<SnapshotFormValues>({
     initialValues: {
