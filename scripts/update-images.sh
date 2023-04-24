b11b1043265928b8eab81905a4c87028de6bbee7
David Edler <david.edler@canonical.com>
2023-04-24 16:24:55 +0200

    feat(images) add check if jq is installed in update script

--- scripts/update-images.sh
+++ scripts/update-images.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if ! jq --version &> /dev/null
+then
+    echo "jq could not be found, please install it: apt install jq"
+    exit
+fi
+
+
 curl https://images.linuxcontainers.org/streams/v1/images.json | jq -c . > public/assets/data/linuxcontainers-images.json
 
 curl http://cloud-images.ubuntu.com/releases/streams/v1/com.ubuntu.cloud:released:download.json | jq -c . > public/assets/data/canonical-images.json
