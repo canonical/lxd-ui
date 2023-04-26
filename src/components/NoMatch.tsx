c3cc96351d9b14ddd58093a79312d401f4f97df7
lorumic <michele.lorusso@canonical.com>
2023-04-26 10:09:16 +0200

    fix(no-match) 404 page overhaul WD-3149

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -1,12 +1,29 @@
 import React, { FC } from "react";
-import { Strip } from "@canonical/react-components";
+import { Row, Col } from "@canonical/react-components";
 
 const NoMatch: FC = () => {
   return (
-    <Strip>
-      <h1>Page not found</h1>
-      <p className="p-heading--4">Sorry, we could not find that page</p>
-    </Strip>
+    <main className="l-main no-match">
+      <Row>
+        <Col size={6} className="col-start-large-4">
+          <h1 className="p-heading--4">404 Page not found</h1>
+          <p>
+            Sorry, we cannot find the page that you are looking for.
+            <br />
+            If you think this is an error in our product, please{" "}
+            <a
+              href="https://github.com/canonical/lxd-ui/issues/new"
+              target="_blank"
+              rel="noreferrer"
+              title="Report a bug"
+            >
+              Report a bug
+            </a>
+            .
+          </p>
+        </Col>
+      </Row>
+    </main>
   );
 };
 
