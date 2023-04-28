b5cab2c47d6c10f223b86f6d8c7d0dffe4465370
lorumic <michele.lorusso@canonical.com>
2023-04-28 14:44:12 +0200

    fix(side-panel) make header and actions sticky with CSS (position)

--- src/components/DetailPanel.tsx
+++ src/components/DetailPanel.tsx
@@ -1,11 +1,9 @@
-import React, { FC, ReactNode, useEffect } from "react";
+import React, { FC, ReactNode } from "react";
 import { Button, Icon } from "@canonical/react-components";
 import classnames from "classnames";
 import Aside from "./Aside";
 import Loader from "./Loader";
 import usePanelParams from "util/usePanelParams";
-import useEventListener from "@use-it/event-listener";
-import { updateMaxHeight } from "util/updateMaxHeight";
 
 interface Props {
   title: string;
@@ -26,36 +24,30 @@ const DetailPanel: FC<Props> = ({
 }) => {
   const panelParams = usePanelParams();
 
-  const updateContentHeight = () => {
-    updateMaxHeight("content-scroll");
-  };
-  useEffect(updateContentHeight, []);
-  useEventListener("resize", updateContentHeight);
-
   return (
     <Aside width="narrow" pinned className="u-hide--medium u-hide--small">
       {isLoading && <Loader />}
       {!isLoading && hasLoadingError && <>Loading failed</>}
       {!hasLoadingError && (
         <div className={classnames("p-panel", "detail-panel", className)}>
-          <div className="p-panel__header">
-            <h2 className="p-panel__title">{title}</h2>
-            <div className="p-panel__controls">
-              <Button
-                appearance="base"
-                className="u-no-margin--bottom"
-                hasIcon
-                onClick={panelParams.clear}
-                aria-label="Close"
-              >
-                <Icon name="close" />
-              </Button>
+          <div className="sticky-wrapper">
+            <div className="p-panel__header">
+              <h2 className="p-panel__title">{title}</h2>
+              <div className="p-panel__controls">
+                <Button
+                  appearance="base"
+                  className="u-no-margin--bottom"
+                  hasIcon
+                  onClick={panelParams.clear}
+                  aria-label="Close"
+                >
+                  <Icon name="close" />
+                </Button>
+              </div>
             </div>
-          </div>
-          <div className="p-panel__content panel-content">
             {actions}
-            <div className="content-scroll">{children}</div>
           </div>
+          <div className="p-panel__content">{children}</div>
         </div>
       )}
     </Aside>
