fd89c7bb2a2204fa1dd2c6723b74a476f80d779a
David Edler <david.edler@canonical.com>
2023-05-10 19:01:51 +0200

    feat(instance) introduce dedicated InstanceIps component

--- src/util/networks.tsx
+++ src/util/networks.tsx
@@ -1,38 +1,13 @@
-import React from "react";
 import { LxdInstance } from "types/instance";
 
 export const getIpAddresses = (
   instance: LxdInstance,
-  family?: string,
-  showLocal = true
+  family: "inet" | "inet6"
 ) => {
   if (!instance.state?.network) return [];
-  const interfaces = Object.entries(instance.state.network).filter(
-    ([key, _value]) => key !== "lo"
-  );
-  const addresses = interfaces.flatMap(([_key, value]) => value.addresses);
-  const filteredAddresses =
-    !family && showLocal
-      ? addresses
-      : addresses.filter(
-          (item) =>
-            (family ? item.family === family : true) &&
-            (showLocal
-              ? true
-              : !(
-                  item.address.startsWith("127") ||
-                  item.address.startsWith("fe80")
-                ))
-        );
-  return filteredAddresses.map((item) => item.address);
-};
-
-export const getIpAddressElements = (instance: LxdInstance, family: string) => {
-  return getIpAddresses(instance, family).map((address) => {
-    return (
-      <div key={address} className="ip u-truncate" title={address}>
-        {address}
-      </div>
-    );
-  });
+  return Object.entries(instance.state.network)
+    .filter(([key, _value]) => key !== "lo")
+    .flatMap(([_key, value]) => value.addresses)
+    .filter((item) => item.family === family)
+    .map((item) => item.address);
 };
