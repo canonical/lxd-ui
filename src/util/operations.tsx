ca226bc76dda0c8040190840de799a7ec7d71dcf
David Edler <david.edler@canonical.com>
2023-07-07 10:47:16 +0200

    fix(instances) notification on instance creation and clear cache

--- src/util/operations.tsx
+++ src/util/operations.tsx
@@ -3,11 +3,13 @@ import { LxdOperation } from "types/operation";
 export const getInstanceName = (operation: LxdOperation): string => {
   // the url can be one of below formats
   // /1.0/instances/<instance_name>
+  // /1.0/instances/<instance_name>?project=<project_name>
   // /1.0/instances/<instance_name>/snapshots/<snapshot_name>
   return (
     operation.resources?.instances
       ?.filter((item) => item.startsWith("/1.0/instances/"))
       .map((item) => item.split("/")[3])
-      .pop() ?? ""
+      .pop()
+      ?.split("?")[0] ?? ""
   );
 };
