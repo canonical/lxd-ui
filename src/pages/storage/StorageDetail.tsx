c1cc6b2a098a6f983be3f347969874ee624477cf
David Edler <david.edler@canonical.com>
2023-08-04 14:20:51 +0200

    feat(storage) improve storage detail page

--- src/pages/storage/StorageDetail.tsx
+++ src/pages/storage/StorageDetail.tsx
@@ -1,20 +1,25 @@
 import React, { FC } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "util/queryKeys";
-import { Row, Strip, useNotify } from "@canonical/react-components";
+import { Row, Tabs, useNotify } from "@canonical/react-components";
 import Loader from "components/Loader";
 import { fetchStoragePool } from "api/storage-pools";
-import StorageSize from "pages/storage/StorageSize";
-import StorageUsedBy from "pages/storage/StorageUsedBy";
 import StorageDetailHeader from "pages/storage/StorageDetailHeader";
 import NotificationRow from "components/NotificationRow";
+import { slugify } from "util/slugify";
+import StorageVolumes from "pages/storage/StorageVolumes";
+import StorageOverview from "pages/storage/StorageOverview";
+
+const TABS: string[] = ["Overview", "Volumes"];
 
 const StorageDetail: FC = () => {
+  const navigate = useNavigate();
   const notify = useNotify();
-  const { name, project } = useParams<{
+  const { name, project, activeTab } = useParams<{
     name: string;
     project: string;
+    activeTab?: string;
   }>();
 
   if (!name) {
@@ -43,6 +48,15 @@ const StorageDetail: FC = () => {
     return <>Loading storage details failed</>;
   }
 
+  const handleTabChange = (newTab: string) => {
+    notify.clear();
+    if (newTab === "overview") {
+      navigate(`/ui/project/${project}/storage/detail/${name}`);
+    } else {
+      navigate(`/ui/project/${project}/storage/detail/${name}/${newTab}`);
+    }
+  };
+
   return (
     <main className="l-main">
       <div className="p-panel instance-detail-page">
@@ -54,45 +68,28 @@ const StorageDetail: FC = () => {
         <div className="p-panel__content">
           <NotificationRow />
           <Row>
-            <table className="storage-detail-table">
-              <tbody>
-                <tr>
-                  <th className="u-text--muted">Name</th>
-                  <td>{storagePool.name}</td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Status</th>
-                  <td>{storagePool.status}</td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Size</th>
-                  <td>
-                    <StorageSize storage={storagePool} />
-                  </td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Source</th>
-                  <td>{storagePool.config?.source ?? "-"}</td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Description</th>
-                  <td>
-                    {storagePool.description ? storagePool.description : "-"}
-                  </td>
-                </tr>
-                <tr>
-                  <th className="u-text--muted">Driver</th>
-                  <td>{storagePool.driver}</td>
-                </tr>
-              </tbody>
-            </table>
-            <h2 className="p-heading--5">Used by</h2>
-            <StorageUsedBy storage={storagePool} project={project} />
-            <Strip className="u-no-padding--bottom">
-              <Link to={`/ui/${project}/storages/${storagePool.name}/volumes`}>
-                Volume details
-              </Link>
-            </Strip>
+            <Tabs
+              links={TABS.map((tab) => ({
+                label: tab,
+                id: slugify(tab),
+                active:
+                  slugify(tab) === activeTab ||
+                  (tab === "Overview" && !activeTab),
+                onClick: () => handleTabChange(slugify(tab)),
+              }))}
+            />
+
+            {!activeTab && (
+              <div role="tabpanel" aria-labelledby="overview">
+                <StorageOverview name={name} project={project} />
+              </div>
+            )}
+
+            {activeTab === "volumes" && (
+              <div role="tabpanel" aria-labelledby="Volumes">
+                <StorageVolumes />
+              </div>
+            )}
           </Row>
         </div>
       </div>
