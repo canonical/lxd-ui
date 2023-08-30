e118a34f420f29b27b74d058fac793f0051696ad
David Edler <david.edler@canonical.com>
2023-08-30 17:47:13 +0200

    fix(images) ensure the arch filter on image selector is set to a supported architecture #437

--- src/pages/images/ImageSelector.tsx
+++ src/pages/images/ImageSelector.tsx
@@ -115,6 +115,10 @@ const ImageSelector: FC<Props> = ({ primaryImage, onSelect, onUpload }) => {
     .sort();
   const variantAll = [...new Set(images.map((item) => item.variant))].sort();
 
+  if (!isLoading && !archAll.includes(arch)) {
+    setArch(archAll[0]);
+  }
+
   const getOptionList: (
     mapper: (item: RemoteImage) => string,
     filter?: (item: RemoteImage) => boolean
