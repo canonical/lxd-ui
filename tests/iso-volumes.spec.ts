9862be8ae72073163a6c9c34be40ae83bcec3fec
David Edler <david.edler@canonical.com>
2023-09-01 08:44:39 +0200

    feat(storage) add tests for iso images

---
+++ tests/iso-volumes.spec.ts
@@ -0,0 +1,39 @@
+import { test } from "@playwright/test";
+import { randomNameSuffix } from "./helpers/name";
+
+const ISO_FILE = "./tests/fixtures/foo.iso";
+
+export const randomIso = (): string => {
+  return `playwright-iso-${randomNameSuffix()}`;
+};
+
+test("upload and delete custom iso", async ({ page }) => {
+  const isoName = randomIso();
+
+  await page.goto("/ui/");
+  await page.getByRole("link", { name: "Storage", exact: true }).click();
+  await page.getByTestId("tab-link-Custom ISOs").click();
+  await page.getByRole("button", { name: "Upload custom ISO" }).click();
+  await page.getByLabel("Local file").setInputFiles(ISO_FILE);
+  await page.getByLabel("Alias").fill(isoName);
+
+  await page.getByRole("button", { name: "Upload", exact: true }).click();
+  await page.getByText(`Image ${isoName} uploaded successfully`).click();
+
+  await page.getByPlaceholder("Search for custom ISOs").fill(isoName);
+  await page.getByRole("button", { name: "Create instance" }).click();
+
+  await page.getByText("YAML configuration").click();
+  await page.getByText("devices:", { exact: true }).click();
+  await page.getByText("iso-volume:", { exact: true }).click();
+  await page.getByText("boot.priority: '10'", { exact: true }).click();
+  await page.getByText(`source: ${isoName}`, { exact: true }).click();
+  await page.getByText("type: disk", { exact: true }).click();
+  await page.getByRole("button", { name: "Cancel", exact: true }).click();
+
+  await page.getByPlaceholder("Search for custom ISOs").fill(isoName);
+  await page.getByText("3 B").click();
+  await page.getByRole("button", { name: "Delete" }).click();
+  await page.getByText("Delete", { exact: true }).click();
+  await page.getByText(`Storage volume ${isoName} deleted.`).click();
+});
