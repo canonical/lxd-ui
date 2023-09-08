2d1455cc101bd83ff42311ee90e7d8ec660f60fc
David Edler <david.edler@canonical.com>
2023-09-08 08:37:48 +0200

    Revert "fix(iso) simplify not on custom image upload"
    This reverts commit cb0c34d1e00b11d0b49f7c3b6ba59c4d301c2007.

--- src/pages/storage/UploadCustomImageHint.tsx
+++ src/pages/storage/UploadCustomImageHint.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
-import { Icon } from "@canonical/react-components";
 
 const UploadCustomImageHint: FC = () => {
   return (
@@ -7,19 +6,31 @@ const UploadCustomImageHint: FC = () => {
       <div className={`p-notification--information`}>
         <div className="p-notification__content">
           <h5 className="p-notification__title">
-            Some image formats need to be modified in order to work with LXD.
+            Image must be prepared with distrobuilder
           </h5>
           <p>
-            <a
-              className="p-notification__action"
-              href="https://discourse.ubuntu.com/t/how-to-install-a-windows-11-vm-using-lxd/28940"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Windows ISO images
-              <Icon className="external-link-icon" name="external-link" />
-            </a>
+            For a Windows image with name <code>WindowsIsoImage.iso</code>, use
+            the command below and upload the resulting{" "}
+            <code>win11.lxd.iso</code> file.
           </p>
+          <pre className="p-code-snippet__block--icon">
+            <code>
+              sudo distrobuilder repack-windows WindowsIsoImage.iso
+              win11.lxd.iso
+            </code>
+          </pre>
+          <div className="p-notification__meta">
+            <div className="p-notification__actions">
+              <a
+                className="p-notification__action"
+                href="https://discourse.ubuntu.com/t/how-to-install-a-windows-11-vm-using-lxd/28940"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Learn how to install a Windows 11 VM using LXD
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </>
