32ebcc96c160a276914b85f1fedcaf40afe6f33a
David Edler <david.edler@canonical.com>
2023-09-18 09:59:35 +0200

    feat(title) add hostname from url to the page title #464

---
+++ src/util/title.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import { useSettings } from "context/useSettings";
+
+export const setTitle = () => {
+  const { data: settings } = useSettings();
+
+  useEffect(() => {
+    const host = settings?.config["user.ui_title"] ?? location.hostname;
+    document.title = `${host} | LXD UI`;
+  }, [settings?.config]);
+};
