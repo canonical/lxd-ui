57371db364a477da0a0e5c5347bce5769146df66
David Edler <david.edler@canonical.com>
2023-09-24 12:38:36 +0200

    fix(storage) add contentClassName to BaseLayout and use it in StorageVolumeForm

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -1,19 +1,26 @@
 import React, { FC, ReactNode } from "react";
+import classnames from "classnames";
 
 interface Props {
   title?: string | ReactNode;
   controls?: ReactNode | null;
   children: ReactNode;
+  contentClassName?: string;
 }
 
-const BaseLayout: FC<Props> = ({ title, controls = null, children }: Props) => {
+const BaseLayout: FC<Props> = ({
+  title,
+  controls = null,
+  children,
+  contentClassName,
+}: Props) => {
   const hasHeader = Boolean(title) || Boolean(controls);
 
   return (
     <main className="l-main">
       <div className="p-panel">
         {hasHeader && (
-          <div className="p-panel__header">
+          <div className={classnames("p-panel__header", contentClassName)}>
             {title && <h1 className="p-panel__title">{title}</h1>}
             {controls && <div className="p-panel__controls">{controls}</div>}
           </div>
