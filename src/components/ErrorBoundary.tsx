16ad1db32191b678e7e64190743e4563f26e5c09
David Edler <david.edler@canonical.com>
2024-01-05 11:54:37 +0100

    feat(errors) add error boundaries WD-8089
    Signed-off-by: David Edler <david.edler@canonical.com>

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from "react";
+import type { PropsWithChildren } from "react";
+import { Component } from "react";
+
+type Props = PropsWithChildren & {
+  fallback: FC<{ error?: Error }>;
+};
+
+type State = {
+  error?: Error;
+  hasError: boolean;
+};
+
+export default class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return {
+      hasError: true,
+      error,
+    };
+  }
+
+  render() {
+    const { error, hasError } = this.state;
+    const { children, fallback: ErrorComponent } = this.props;
+
+    return hasError ? <ErrorComponent error={error} /> : <>{children}</>;
+  }
+}
