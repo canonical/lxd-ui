46296dc83da81aa898642369f7a810c6948df3f3
David Edler <david.edler@canonical.com>
2024-01-18 19:26:54 +0100

    feat(docs) use documentation links from objects.inv.txt file shipped with lxd in favor of hard coding the link replacements WD-8064
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/pages/settings/ConfigFieldDescription.tsx
+++ src/pages/settings/ConfigFieldDescription.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react";
 import { useDocs } from "context/useDocs";
 import { configDescriptionToHtml } from "util/config";
+import { useQuery } from "@tanstack/react-query";
+import { fetchDocObjects } from "api/server";
 
 interface Props {
   description?: string;
@@ -9,12 +11,20 @@ interface Props {
 
 const ConfigFieldDescription: FC<Props> = ({ description, className }) => {
   const docBaseLink = useDocs();
+  const objectsInvTxt = useQuery({
+    queryKey: ["documentation/objects.inv.txt"],
+    queryFn: fetchDocObjects,
+  });
 
   return description ? (
     <p
       className={className}
       dangerouslySetInnerHTML={{
-        __html: configDescriptionToHtml(description, docBaseLink),
+        __html: configDescriptionToHtml(
+          description,
+          docBaseLink,
+          objectsInvTxt.data,
+        ),
       }}
     ></p>
   ) : null;
--- src/util/config.tsx
+++ src/util/config.tsx
@@ -24,79 +24,42 @@ export const toConfigFields = (
   return result;
 };
 
-const docLinkReplacements = {
-  "{config:option}`instance-raw:raw.idmap":
-    "/reference/instance_options/#instance-raw:raw.idmap",
-  "{config:option}`instance-raw:raw.lxc":
-    "/reference/instance_options/#instance-raw:raw.lxc",
-  "{config:option}`instance-raw:raw.qemu":
-    "/reference/instance_options/#instance-raw:raw.qemu",
-  "{config:option}`instance-resource-limits:limits.cpu":
-    "/reference/instance_options/#instance-resource-limits:limits.cpu",
-  "{config:option}`instance-resource-limits:limits.memory":
-    "/reference/instance_options/#instance-resource-limits:limits.memory",
-  "{config:option}`instance-resource-limits:limits.processes":
-    "/reference/instance_options/#instance-resource-limits:limits.processes",
-  "{config:option}`instance-security:security.csm":
-    "/reference/instance_options/#instance-security:security.csm",
-  "{config:option}`instance-security:security.idmap.isolated":
-    "/reference/instance_options/#instance-security:security.idmap.isolated",
-  "{config:option}`instance-security:security.nesting":
-    "/reference/instance_options/#instance-security:security.nesting",
-  "{config:option}`instance-security:security.privileged":
-    "/reference/instance_options/#instance-security:security.privileged",
-  "{config:option}`instance-security:security.secureboot":
-    "/reference/instance_options/#instance-security:security.secureboot",
-  "{config:option}`project-restricted:restricted.devices.disk":
-    "/reference/projects/#project-restricted:restricted.devices.disk",
-  "{config:option}`project-restricted:restricted.devices.nic":
-    "/reference/projects/#project-restricted:restricted.devices.nic",
-  "{ref}`cluster-evacuate": "/howto/cluster_manage/#cluster-evacuate",
-  "{ref}`cluster-https-address":
-    "/howto/cluster_config_networks/#cluster-https-address",
-  "{ref}`clustering-instance-placement":
-    "/explanation/clustering/#clustering-instance-placement",
-  "{ref}`clustering-instance-placement-scriptlet":
-    "/explanation/clustering/#clustering-instance-placement-scriptlet",
-  "{ref}`dev-lxd": "/dev-lxd/#dev-lxd",
-  "{ref}`howto-storage-buckets":
-    "/howto/storage_buckets/#howto-storage-buckets",
-  "{ref}`instance-options-limits-cpu":
-    "/reference/instance_options/#instance-options-limits-cpu",
-  "{ref}`instance-options-limits-cpu-container":
-    "/reference/instance_options/#instance-options-limits-cpu-container",
-  "{ref}`instance-options-qemu":
-    "/reference/instance_options/#instance-options-qemu",
-  "{ref}`instance-options-snapshots-names":
-    "/reference/instance_options/#instance-options-snapshots-names",
-  "{ref}`instances-limit-units":
-    "/reference/instance_units/#instances-limit-units",
-  "{ref}`metrics": "/metrics/#metrics",
-  "{ref}`network-bgp": "/howto/network_bgp/#network-bgp",
-  "{ref}`network-dns-server": "/howto/network_zones/#network-dns-server",
-  "{ref}`server-expose": "/howto/server_expose/#server-expose",
-};
-
-export const configDescriptionToHtml = (input: string, docBaseLink: string) => {
+export const configDescriptionToHtml = (
+  input: string,
+  docBaseLink: string,
+  objectsInvTxt?: string[],
+) => {
   // special characters
   let result = input
     .replaceAll("<", "&lt;")
     .replaceAll(">", "&gt;")
-    .replaceAll("\n", "<br>");
+    .replaceAll("\n", "<br>")
+    .replaceAll("```", "");
 
   // documentation links
-  Object.entries(docLinkReplacements).forEach(([key, value]) => {
-    const href = `${docBaseLink}${value}`;
-    const linkText = key
-      .substring(key.lastIndexOf("`") + 1)
-      .split(":")
-      .pop()
-      ?.replaceAll("-", " ");
-    result = result.replaceAll(
-      key + "`",
-      `<a href="${href}" target="_blank" rel="noreferrer">${linkText}</a>`,
-    );
-  });
+  if (objectsInvTxt) {
+    // tags like {ref}`instance-options-qemu` can be in the input string
+    const linkTags = input.match(/{(config|ref|config:option)}`[a-z-:.]+`/g);
+    linkTags?.map((tag) => {
+      const token = tag
+        .substring(tag.indexOf("`") + 1, tag.lastIndexOf("`"))
+        ?.split(":")
+        .pop();
+      if (!token) {
+        return;
+      }
+      // find line with token wrapped in spaces to avoid matching partial tokens
+      const line = objectsInvTxt.find((item) => item.includes(` ${token} `));
+      if (!line) {
+        return;
+      }
+      const docPath = line.split(": ")[1];
+      const linkText = token.replaceAll("-", " ");
+      const link = `<a href="${docBaseLink}/${docPath}" target="_blank" rel="noreferrer">${linkText}</a>`;
+
+      result = result.replaceAll(tag, link);
+    });
+  }
 
   // code blocks
   let count = 0;
