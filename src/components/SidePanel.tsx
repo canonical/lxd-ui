6cef4fdcb277ce373878654f14a86d1d9250978f
Mason Hu <mason.hu@canonical.com>
2024-03-13 19:37:42 +0200

    feat: refactor side panels and enabled overlay panels
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

---
+++ src/components/SidePanel.tsx
@@ -0,0 +1,143 @@
+import { FC, PropsWithChildren, ReactNode } from "react";
+import Loader from "components/Loader";
+import classnames from "classnames";
+import { Spinner } from "@canonical/react-components";
+
+interface CommonProps {
+  className?: string;
+}
+
+// Header components
+const HeaderControls: FC<PropsWithChildren & CommonProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div className={classnames("p-panel__controls", className)}>{children}</div>
+  );
+};
+
+const HeaderTitle: FC<PropsWithChildren & CommonProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <h2 className={classnames("p-panel__title", className)}>{children}</h2>
+  );
+};
+
+const Sticky: FC<PropsWithChildren & CommonProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div className={classnames("sticky-wrapper", className)}>{children}</div>
+  );
+};
+
+const Header: FC<PropsWithChildren & CommonProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div className={classnames("p-panel__header", className)}>{children}</div>
+  );
+};
+
+// Panel content components
+const Container: FC<PropsWithChildren & CommonProps> = ({
+  children,
+  className,
+}) => {
+  return <div className={classnames("p-panel", className)}>{children}</div>;
+};
+
+const Content: FC<PropsWithChildren & CommonProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div className={classnames("p-panel__content", className)}>{children}</div>
+  );
+};
+
+// Footer components
+const Footer: FC<PropsWithChildren & CommonProps> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div className={classnames("panel-footer", className)}>
+      <hr />
+      {children}
+    </div>
+  );
+};
+
+interface SidePanelProps {
+  isOverlay?: boolean;
+  isSplit?: boolean;
+  children: ReactNode;
+  loading: boolean;
+  hasError: boolean;
+  className?: string;
+  width?: "narrow" | "wide";
+  pinned?: boolean;
+}
+
+const SidePanelComponent: FC<SidePanelProps> = ({
+  children,
+  isOverlay,
+  isSplit = false,
+  loading = false,
+  hasError,
+  className,
+  width,
+  pinned,
+}) => {
+  return (
+    <aside
+      className={classnames("l-aside", className, {
+        "is-narrow": width === "narrow",
+        "is-wide": width === "wide",
+        "is-pinned": pinned,
+        "is-split": isSplit,
+        "is-overlay": isOverlay,
+      })}
+      aria-label="Side panel"
+    >
+      {loading ? (
+        <div className="loading">
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          {loading && <Loader />}
+          {!loading && hasError && <>Loading failed</>}
+          {!hasError && children}
+        </>
+      )}
+    </aside>
+  );
+};
+
+type SidePanelComponents = FC<SidePanelProps> & {
+  Header: FC<PropsWithChildren & CommonProps>;
+  HeaderTitle: FC<PropsWithChildren & CommonProps>;
+  HeaderControls: FC<PropsWithChildren & CommonProps>;
+  Sticky: FC<PropsWithChildren & CommonProps>;
+  Container: FC<PropsWithChildren & CommonProps>;
+  Content: FC<PropsWithChildren & CommonProps>;
+  Footer: FC<PropsWithChildren & CommonProps>;
+};
+
+const SidePanel = SidePanelComponent as SidePanelComponents;
+SidePanel.Header = Header;
+SidePanel.HeaderTitle = HeaderTitle;
+SidePanel.HeaderControls = HeaderControls;
+SidePanel.Sticky = Sticky;
+SidePanel.Container = Container;
+SidePanel.Content = Content;
+SidePanel.Footer = Footer;
+
+export default SidePanel;
---
+++ src/pages/profiles/ProfileDetailPanelContent.tsx
@@ -0,0 +1,85 @@
+import { FC } from "react";
+import { LxdProfile } from "types/profile";
+import ProfileLink from "./ProfileLink";
+import { getProfileInstances } from "util/usedBy";
+import ProfileNetworkList from "./ProfileNetworkList";
+import ProfileStorageList from "./ProfileStorageList";
+import ProfileInstances from "./ProfileInstances";
+import { LxdProject } from "types/project";
+import { isProjectWithProfiles } from "util/projects";
+
+interface Props {
+  profile: LxdProfile;
+  project: LxdProject;
+}
+
+const ProfileDetailPanelContent: FC<Props> = ({ profile, project }) => {
+  const isDefaultProject = project.name === "default";
+  const usageCount = getProfileInstances(
+    project.name,
+    isDefaultProject,
+    profile?.used_by,
+  ).length;
+
+  const featuresProfiles = isProjectWithProfiles(project);
+
+  return (
+    <table className="u-table-layout--auto u-no-margin--bottom">
+      <tbody>
+        <tr>
+          <th className="u-text--muted">Name</th>
+          <td>
+            <ProfileLink
+              profile={{ name: profile.name, project: project.name }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <th className="u-text--muted">Description</th>
+          <td>{profile.description ? profile.description : "-"}</td>
+        </tr>
+        <tr>
+          <th className="u-text--muted last-of-section">Defined in</th>
+          <td>{featuresProfiles ? "Current" : "Default"} project</td>
+        </tr>
+        <tr className="u-no-border">
+          <th colSpan={2}>
+            <h3 className="p-muted-heading p-heading--5">Devices</h3>
+          </th>
+        </tr>
+        <tr className="u-no-border list-wrapper">
+          <th className="u-text--muted">Networks</th>
+          <td>
+            <ProfileNetworkList profile={profile} />
+          </td>
+        </tr>
+        <tr className="u-no-border list-wrapper">
+          <th className="u-text--muted last-of-section">Storage</th>
+          <td>
+            <ProfileStorageList profile={profile} />
+          </td>
+        </tr>
+        <tr className="used-by-header">
+          <th colSpan={2}>
+            <h3 className="p-muted-heading p-heading--5">
+              Used by ({usageCount})
+            </h3>
+          </th>
+        </tr>
+        {usageCount > 0 ? (
+          <ProfileInstances
+            profile={profile}
+            project={project.name}
+            headingClassName="u-text--muted"
+          />
+        ) : (
+          <tr>
+            <td colSpan={2}>No items found.</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+export default ProfileDetailPanelContent;
