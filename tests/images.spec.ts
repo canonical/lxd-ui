1056b90ed9d186a3f4b2123a98c90ece67037faa
David Edler <david.edler@canonical.com>
2024-04-09 17:18:36 +0200

    chore(test) add custom image test WD-10108
    Signed-off-by: David Edler <david.edler@canonical.com>

---
+++ tests/images.spec.ts
@@ -0,0 +1,20 @@
+import { test } from "./fixtures/lxd-test";
+import { deleteInstance, randomInstanceName } from "./helpers/instances";
+
+test("search for custom image and create an instance from it", async ({
+  page,
+}) => {
+  const customInstance = randomInstanceName();
+  const image = "my-custom-image"; // this is created in pr.yaml and coverage.yaml
+
+  await page.goto("/ui/");
+  await page.getByRole("link", { name: "Images", exact: true }).click();
+  await page.getByPlaceholder("Search").click();
+  await page.getByPlaceholder("Search").fill(image);
+  await page.getByRole("button", { name: "Create instance" }).first().click();
+  await page.getByLabel("Instance name").fill(customInstance);
+  await page.getByRole("button", { name: "Create", exact: true }).click();
+  await page.waitForSelector(`text=Created instance ${customInstance}.`);
+
+  await deleteInstance(page, customInstance);
+});
