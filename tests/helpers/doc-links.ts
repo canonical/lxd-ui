e23ea01feaa59c6f3d35b2de5290085994d7e186
Nkeiruka <nkeiruka.whenu@canonical.com>
2024-06-10 22:11:46 +0100

    chore(tests): added e2e testing for doc links
    Signed-off-by: Nkeiruka <nkeiruka.whenu@canonical.com>

---
+++ tests/helpers/doc-links.ts
@@ -0,0 +1,12 @@
+import { Page } from "@playwright/test";
+import { expect } from "../fixtures/lxd-test";
+
+export const validateLink = async (
+  page: Page,
+  linkText: string,
+  link: string,
+) => {
+  await expect(
+    page.getByRole("link", { name: linkText, exact: true }).first(),
+  ).toHaveAttribute("href", link);
+};
