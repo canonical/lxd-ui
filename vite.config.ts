a37bdc2c51c16f920050df856fd08ee1aa9e09ad
Mason Hu <mason.hu@canonical.com>
2025-01-17 19:44:00 +0200

    chore: make vite dev server port configurable
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+
+// Load .env.local if it exists to override default environment variables
+dotenv.config({ path: ".env" });
+if (fs.existsSync(".env.local")) {
+  dotenv.config({ path: ".env.local" });
+}
 
 export default defineConfig({
   css: {
@@ -13,7 +21,8 @@ export default defineConfig({
   },
   plugins: [tsconfigPaths(), react()],
   server: {
-    port: 3000,
+    port: process.env.VITE_PORT ? Number(process.env.VITE_PORT) : 3000,
+    strictPort: true,
     proxy: {
       "/ui/assets": {
         target: "https://localhost:8407/",
