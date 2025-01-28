4de56557123ae073604c8c0600e0d76ace56486e
David Edler <david.edler@canonical.com>
2025-01-28 09:53:22 +0100

    feat(metrics) show cpu and disk usage in instance list. distinguish memory cached and used. fixes #928 #1062
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/components/Meter.tsx
+++ src/components/Meter.tsx
@@ -1,17 +1,36 @@
 import { FC } from "react";
+import classnames from "classnames";
 
 interface Props {
   percentage: number;
+  secondaryPercentage?: number;
   text: string;
 }
 
-const Meter: FC<Props> = ({ percentage, text }: Props) => {
+const Meter: FC<Props> = ({
+  percentage,
+  secondaryPercentage = 0,
+  text,
+}: Props) => {
   return (
     <>
       <div className="p-meter u-no-margin--bottom">
-        <div style={{ width: `${percentage}%` }} />
+        <div
+          style={{ width: `max(${percentage}%, 5px)` }}
+          className={classnames({
+            "has-next-sibling": secondaryPercentage > 0,
+          })}
+        />
+        {secondaryPercentage ? (
+          <div
+            className="has-previous-sibling"
+            style={{ width: `${secondaryPercentage}%` }}
+          />
+        ) : null}
       </div>
-      <div className="p-text--small u-no-margin--bottom">{text}</div>
+      <div className="p-text--small u-no-margin--bottom u-text--muted">
+        {text}
+      </div>
     </>
   );
 };
