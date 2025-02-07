5651dbc4e47e4945c9811e3517bc203b41c29623
Mason Hu <mason.hu@canonical.com>
2025-02-07 13:51:45 +0200

    feat: disable create storage volume button in attach custom volume modal when user has no permission
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- src/util/entitlements/projects.tsx
+++ src/util/entitlements/projects.tsx
@@ -1,33 +1,15 @@
 import { useAuth } from "context/auth";
-import { hasEntitlement, hasEntitlementSet } from "./helpers";
+import { hasEntitlement } from "./helpers";
 import { LxdProject } from "types/project";
-import { useProject } from "context/project";
 
 export const useProjectEntitlements = () => {
   const { isFineGrained } = useAuth();
-  const { project: currentProject } = useProject();
-
-  const validProject = project || currentProject;
 
   const canCreateInstances = (project?: LxdProject) =>
     hasEntitlement(
       isFineGrained,
       "can_create_instances",
-      validProject?.access_entitlements,
-    );
-
-  const canCreateImages = () =>
-    hasEntitlement(
-      isFineGrained,
-      "can_create_images",
-      validProject?.access_entitlements,
-    );
-
-  const canCreateImageAliases = () =>
-    hasEntitlement(
-      isFineGrained,
-      "can_create_image_aliases",
-      validProject?.access_entitlements,
+      project?.access_entitlements,
     );
 
   const canCreateImages = (project?: LxdProject) =>
@@ -44,21 +26,17 @@ export const useProjectEntitlements = () => {
       project?.access_entitlements,
     );
 
+  const canCreateStorageVolumes = (project?: LxdProject) =>
+    hasEntitlement(
+      isFineGrained,
+      "can_create_storage_volumes",
+      project?.access_entitlements,
+    );
+
   return {
     canCreateInstances,
     canCreateImages,
     canCreateImageAliases,
-  };
-};
-
-export const useProjectEntitlementSet = (projects: LxdProject[]) => {
-  const { isFineGrained } = useAuth();
-
-  return {
-    canCreateInstancesSet: hasEntitlementSet(
-      isFineGrained,
-      "can_create_instances",
-      projects,
-    ),
+    canCreateStorageVolumes,
   };
 };
