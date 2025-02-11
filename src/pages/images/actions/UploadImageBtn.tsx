59dec7fe07b6e67e88d8af8650a6ba72d55cf22f
Mason Hu <mason.hu@canonical.com>
2025-02-11 14:26:57 +0200

    feat: disable upload image button when user has no permission
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- src/pages/images/actions/UploadImageBtn.tsx
+++ src/pages/images/actions/UploadImageBtn.tsx
@@ -3,6 +3,7 @@ import { Button, Icon } from "@canonical/react-components";
 import { usePortal } from "@canonical/react-components";
 import UploadImageForm from "./forms/UploadImageForm";
 import { useSmallScreen } from "context/useSmallScreen";
+import { useProjectEntitlements } from "util/entitlements/projects";
 
 interface Props {
   project: string;
@@ -11,6 +12,7 @@ interface Props {
 const UploadImageBtn: FC<Props> = ({ project }) => {
   const { openPortal, closePortal, isOpen, Portal } = usePortal();
   const isSmallScreen = useSmallScreen();
+  const { canCreateImages } = useProjectEntitlements();
 
   return (
     <>
@@ -23,6 +25,10 @@ const UploadImageBtn: FC<Props> = ({ project }) => {
         className="u-no-margin--bottom"
         onClick={openPortal}
         hasIcon={!isSmallScreen}
+        disabled={!canCreateImages()}
+        title={
+          canCreateImages() ? "" : "You do not have permission to create images"
+        }
       >
         {!isSmallScreen && <Icon name="upload" />}
         <span>Upload image</span>
