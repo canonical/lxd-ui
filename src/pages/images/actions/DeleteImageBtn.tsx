bcdf80e45be0277ba3da531ee5bb94567429b702
Mason Hu <mason.hu@canonical.com>
2025-02-11 14:54:04 +0200

    feat: disable single image deletion if user has no permission
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- src/pages/images/actions/DeleteImageBtn.tsx
+++ src/pages/images/actions/DeleteImageBtn.tsx
@@ -7,6 +7,7 @@ import { ConfirmationButton, Icon } from "@canonical/react-components";
 import { useEventQueue } from "context/eventQueue";
 import { useToastNotification } from "context/toastNotificationProvider";
 import ResourceLabel from "components/ResourceLabel";
+import { useImageEntitlements } from "util/entitlements/images";
 
 interface Props {
   image: LxdImage;
@@ -18,6 +19,7 @@ const DeleteImageBtn: FC<Props> = ({ image, project }) => {
   const toastNotify = useToastNotification();
   const [isLoading, setLoading] = useState(false);
   const queryClient = useQueryClient();
+  const { canDeleteImage } = useImageEntitlements(image);
 
   const description = image.properties?.description ?? image.fingerprint;
 
@@ -67,12 +69,14 @@ const DeleteImageBtn: FC<Props> = ({ image, project }) => {
             This action cannot be undone, and can result in data loss.
           </p>
         ),
-        confirmButtonLabel: "Delete",
+        confirmButtonLabel: canDeleteImage()
+          ? "Delete"
+          : "You do not have permission to delete this image",
         onConfirm: handleDelete,
       }}
       className="has-icon"
       appearance="base"
-      disabled={isLoading}
+      disabled={isLoading || !canDeleteImage()}
       shiftClickEnabled
       showShiftClickHint
     >
