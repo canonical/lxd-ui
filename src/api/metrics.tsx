e9f7e9adeba1c733ee4eda507b5708c538a41a57
David Edler <david.edler@canonical.com>
2025-02-13 17:31:42 +0100

    feat(metrics) attach fine grained permission control to metrics query
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/api/metrics.tsx
+++ src/api/metrics.tsx
@@ -2,6 +2,8 @@ import parsePrometheusTextFormat from "parse-prometheus-text-format";
 import type { LxdMetricGroup } from "types/metrics";
 
 export const fetchMetrics = (target: string): Promise<LxdMetricGroup[]> => {
+  // in a simple and non-clustered environment, the LXD api responds
+  // with instance.location as "none". Handle it, to avoid sending an invalid target
   const params = target === "none" ? "" : `?target=${target}`;
 
   return new Promise((resolve, reject) => {
--- src/context/useMetrics.tsx
+++ src/context/useMetrics.tsx
@@ -3,16 +3,18 @@ import { useAuth } from "./auth";
 import { queryKeys } from "util/queryKeys";
 import { fetchMetrics } from "api/metrics";
 import { LxdMetricGroup } from "types/metrics";
+import { useServerEntitlements } from "util/entitlements/server";
 
 export const useMetrics = (
   location: string,
 ): UseQueryResult<LxdMetricGroup[]> => {
-  const { isRestricted } = useAuth();
+  const { isRestricted, isFineGrained } = useAuth();
+  const { canViewMetrics } = useServerEntitlements();
 
   return useQuery({
     queryKey: [queryKeys.metrics, location],
     queryFn: () => fetchMetrics(location),
     refetchInterval: 15 * 1000, // 15 seconds
-    enabled: !isRestricted,
+    enabled: !isRestricted && isFineGrained !== null && canViewMetrics(),
   });
 };
