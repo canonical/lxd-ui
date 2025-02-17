0cf3fae7c6a490aabccd6005ea63ffd928624d88
Mason Hu <mason.hu@canonical.com>
2025-02-17 13:05:20 +0200

    feat: disable bulk edit identity groups button when user has no permission
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- src/pages/permissions/actions/EditIdentityGroupsBtn.tsx
+++ src/pages/permissions/actions/EditIdentityGroupsBtn.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import { Button, ButtonProps } from "@canonical/react-components";
 import type { LxdIdentity } from "types/permissions";
 import usePanelParams from "util/usePanelParams";
+import { useIdentityEntitlements } from "util/entitlements/identities";
+import { pluralize } from "util/instanceBulkActions";
 
 interface Props {
   identities: LxdIdentity[];
@@ -13,20 +15,38 @@ const EditIdentityGroupsBtn: FC<Props & ButtonProps> = ({
   className,
   ...buttonProps
 }) => {
+  const { canEditIdentity } = useIdentityEntitlements();
   const panelParams = usePanelParams();
   const buttonText =
     identities.length > 1
       ? `Modify groups for ${identities.length} identities`
       : "Modify groups";
 
+  const restrictedIdentities = identities.filter(
+    (identity) => !canEditIdentity(identity),
+  );
+
+  const getRestrictedWarning = () => {
+    const test = restrictedIdentities
+      .map((identity) => `\n- ${identity.name}`)
+      .join("");
+    return `You do not have permission to modify ${restrictedIdentities.length > 1 ? "some of the selected" : "the selected"} ${pluralize("identity", restrictedIdentities.length)}:${test}`;
+  };
+
   return (
     <>
       <Button
         onClick={() => panelParams.openIdentityGroups()}
         aria-label="Modify groups"
-        title="Modify groups"
+        title={
+          restrictedIdentities.length ? getRestrictedWarning() : "Modify groups"
+        }
         className={className}
-        disabled={!identities.length || !!panelParams.panel}
+        disabled={
+          !!restrictedIdentities.length ||
+          !identities.length ||
+          !!panelParams.panel
+        }
         {...buttonProps}
       >
         {buttonText}
