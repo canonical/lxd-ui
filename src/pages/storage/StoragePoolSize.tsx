59a64d9d5bc85fb4aeecbee2e8ba4ebf06fbb215
Nkeiruka <nkeiruka.whenu@canonical.com>
2025-02-21 14:40:06 +0000

    feat: [WD-18263] CMS Storage pool usage
    Signed-off-by: Nkeiruka <nkeiruka.whenu@canonical.com>

--- src/pages/storage/StoragePoolSize.tsx
+++ src/pages/storage/StoragePoolSize.tsx
@@ -1,10 +1,20 @@
 import { FC } from "react";
-import { fetchStoragePoolResources } from "api/storage-pools";
-import { useQuery } from "@tanstack/react-query";
-import { queryKeys } from "util/queryKeys";
 import type { LxdStoragePool } from "types/storage";
 import { humanFileSize } from "util/helpers";
 import Meter from "components/Meter";
+import { useClusteredStoragePoolResources } from "context/useStoragePools";
+import { isClusteredServer } from "util/settings";
+import { useSettings } from "context/useSettings";
+import { useQuery } from "@tanstack/react-query";
+import { queryKeys } from "util/queryKeys";
+import { fetchStoragePoolResources } from "api/storage-pools";
+import ResourceLink from "components/ResourceLink";
+import {
+  btrfsDriver,
+  dirDriver,
+  lvmDriver,
+  zfsDriver,
+} from "util/storageOptions";
 
 interface Props {
   pool: LxdStoragePool;
@@ -12,27 +22,70 @@ interface Props {
 }
 
 const StoragePoolSize: FC<Props> = ({ pool, hasMeterBar }) => {
-  const { data: resources } = useQuery({
+  const { data: clusteredPoolResources = [] } =
+    useClusteredStoragePoolResources(pool.name);
+  const { data: settings } = useSettings();
+  const sizeSpecificDrivers = [btrfsDriver, dirDriver, lvmDriver, zfsDriver];
+  const hasMemberSpecificSize =
+    sizeSpecificDrivers.includes(pool.driver) && isClusteredServer(settings);
+  const isClustered = isClusteredServer(settings);
+
+  const { data: poolResources } = useQuery({
     queryKey: [queryKeys.storage, pool.name, queryKeys.resources],
     queryFn: () => fetchStoragePoolResources(pool.name),
+    enabled: !hasMemberSpecificSize,
   });
+  const resourceList = hasMemberSpecificSize
+    ? clusteredPoolResources
+    : [poolResources];
 
-  if (!resources) {
-    return <>{pool.config?.size}</>;
-  }
-
-  const total = resources.space.total;
-  const used = resources.space.used || 0;
-
-  if (!hasMeterBar) {
-    return `${humanFileSize(used)} of ${humanFileSize(total)} used`;
+  if (!hasMeterBar && hasMemberSpecificSize) {
+    return "Cluster member dependent";
   }
 
   return (
-    <Meter
-      percentage={(100 / total) * used || 0}
-      text={`${humanFileSize(used)} of ${humanFileSize(total)} used`}
-    />
+    <>
+      {resourceList.map((poolResource, index) => {
+        if (!poolResource) {
+          return <>{pool.config?.size}</>;
+        }
+
+        const total = poolResource.space.total;
+        const used = poolResource.space.used || 0;
+
+        if (!hasMeterBar) {
+          return (
+            <div key={index}>
+              {`${humanFileSize(used)} of ${humanFileSize(total)} used`}
+            </div>
+          );
+        }
+
+        return (
+          <div className="storage-pool-meter-container" key={index}>
+            {isClustered && (
+              <ResourceLink
+                to="/ui/cluster"
+                type={
+                  hasMemberSpecificSize ? "cluster-member" : "cluster-group"
+                }
+                value={
+                  hasMemberSpecificSize && poolResource.memberName
+                    ? poolResource.memberName
+                    : "Cluster wide"
+                }
+              />
+            )}
+
+            <Meter
+              key={index}
+              percentage={(100 / total) * used || 0}
+              text={`${humanFileSize(used)} of ${humanFileSize(total)} used`}
+            />
+          </div>
+        );
+      })}
+    </>
   );
 };
 
