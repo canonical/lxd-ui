7f6a8de386ed816d733ef015befd0a6c320e9f9c
Nkeiruka <nkeiruka.whenu@canonical.com>
2025-03-11 16:16:28 +0000

    feat: [WD-19725] TLS Onboarding Refinement
    Signed-off-by: Nkeiruka <nkeiruka.whenu@canonical.com>

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
-import { Button, Icon } from "@canonical/react-components";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Icon } from "@canonical/react-components";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "context/auth";
 import Loader from "components/Loader";
 import { useSettings } from "context/useSettings";
@@ -8,7 +8,6 @@ import CustomLayout from "components/CustomLayout";
 
 const Login: FC = () => {
   const { isAuthenticated, isAuthLoading } = useAuth();
-  const navigate = useNavigate();
   const { data: settings } = useSettings();
   const hasOidc = settings?.auth_methods?.includes("oidc");
 
@@ -20,49 +19,31 @@ const Login: FC = () => {
     return <Navigate to="/ui" replace={true} />;
   }
 
+  if (!hasOidc) {
+    return <Navigate to="/ui/login/certificate-generate" replace={true} />;
+  }
+
   return (
-    <CustomLayout>
-      <div className="empty-state login">
-        <Icon name="pods" className="empty-state-icon lxd-icon" />
+    <CustomLayout contentClassName="login">
+      <div className="empty-state login-page">
+        <Icon name="pods" className="login-ui-icon lxd-icon" />
         <h1 className="p-heading--4 u-sv-2">Login</h1>
         {hasOidc && (
           <>
             <p className="u-sv1">Choose your login method</p>
-            <a className="p-button--positive" href="/oidc/login">
-              Login with SSO
-            </a>
-            <h2 className="p-heading--5 u-sv-2">Other methods</h2>
-            <div>
-              Either{" "}
-              <Link to="/ui/login/certificate-generate">
-                create a new certificate
-              </Link>
+            <div className="auth-container">
+              <a className="p-button--positive has-icon" href="/oidc/login">
+                <Icon name="security" light />
+                <span>Login with SSO</span>
+              </a>
+              <a
+                className="p-button has-icon"
+                href="/ui/login/certificate-generate"
+              >
+                <Icon name="certificate" className="auth-button-icon" />
+                <span>Login with TLS</span>
+              </a>
             </div>
-            <div>
-              Or{" "}
-              <Link to="/ui/login/certificate-add">
-                use an existing certificate
-              </Link>{" "}
-              already added to your browser
-            </div>
-          </>
-        )}
-        {!hasOidc && (
-          <>
-            <p className="u-sv1">Certificate selection</p>
-            <Button
-              appearance={"positive"}
-              onClick={async () => navigate("/ui/login/certificate-generate")}
-            >
-              Create a new certificate
-            </Button>
-            <p>
-              Or{" "}
-              <Link to="/ui/login/certificate-add">
-                use an existing certificate
-              </Link>{" "}
-              already added to your browser
-            </p>
           </>
         )}
       </div>
