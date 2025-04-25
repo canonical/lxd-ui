d6f0ba7b5cf815e0e1830f42435a430260cb3c96
David Edler <david.edler@canonical.com>
2025-04-25 21:13:42 +0200

    chore(ClusterMemberSelector) remove duplicate and confusing setMember callback
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/pages/cluster/ClusterMemberSelector.tsx
+++ src/pages/cluster/ClusterMemberSelector.tsx
@@ -1,37 +1,21 @@
 import type { SelectProps } from "@canonical/react-components";
 import { Select } from "@canonical/react-components";
-import { useQuery } from "@tanstack/react-query";
-import { fetchClusterMembers } from "api/cluster";
 import type { FC } from "react";
-import { useEffect } from "react";
-import { queryKeys } from "util/queryKeys";
 import { useIsClustered } from "context/useIsClustered";
+import { useClusterMembers } from "context/useClusterMembers";
 
 interface Props {
-  setMember?: (member: string) => void;
   disableReason?: string;
 }
 
 const ClusterMemberSelector: FC<SelectProps & Props> = ({
   label,
   disabled,
-  setMember,
   disableReason,
   ...props
 }) => {
   const isClustered = useIsClustered();
-  const { data: clusterMembers = [], isLoading: clusterMembersLoading } =
-    useQuery({
-      queryKey: [queryKeys.cluster],
-      queryFn: fetchClusterMembers,
-      enabled: isClustered,
-    });
-
-  useEffect(() => {
-    if (clusterMembers.length > 0 && setMember) {
-      setMember(clusterMembers[0].server_name);
-    }
-  }, [clusterMembers]);
+  const { data: clusterMembers = [], isLoading } = useClusterMembers();
 
   return isClustered ? (
     <Select
@@ -43,7 +27,7 @@ const ClusterMemberSelector: FC<SelectProps & Props> = ({
           value: clusterMember.server_name,
         };
       })}
-      disabled={disabled || clusterMembersLoading || !!disableReason}
+      disabled={disabled || isLoading || !!disableReason}
       help={disableReason ?? props.help}
     />
   ) : null;
