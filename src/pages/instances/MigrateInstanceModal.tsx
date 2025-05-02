b85be8f6dc0859586d9191e2284dc9a3973fbc55
David Edler <david.edler@canonical.com>
2025-05-02 18:48:21 +0200

    fix(instance) combined instance migration
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/pages/instances/MigrateInstanceModal.tsx
+++ src/pages/instances/MigrateInstanceModal.tsx
@@ -1,15 +1,35 @@
 import type { FC, KeyboardEvent } from "react";
-import { useState } from "react";
-import { Modal } from "@canonical/react-components";
+import {
+  ActionButton,
+  Button,
+  Form,
+  Input,
+  Modal,
+  Select,
+  useToastNotification,
+} from "@canonical/react-components";
 import type { LxdInstance } from "types/instance";
-import FormLink from "components/FormLink";
-import InstanceClusterMemberMigration from "./InstanceClusterMemberMigration";
-import BackLink from "components/BackLink";
-import InstanceStoragePoolMigration from "./InstanceStoragePoolMigration";
-import type { MigrationType } from "util/instanceMigration";
-import { useInstanceMigration } from "util/instanceMigration";
-import InstanceProjectMigration from "pages/instances/InstanceProjectMigration";
 import { useIsClustered } from "context/useIsClustered";
+import ClusterMemberSelector from "pages/cluster/ClusterMemberSelector";
+import StoragePoolSelector from "pages/storage/StoragePoolSelector";
+import { useFormik } from "formik";
+import type { LxdDiskDevice } from "types/device";
+import InstanceLinkChip from "pages/instances/InstanceLinkChip";
+import { migrateInstance } from "api/instances";
+import { queryKeys } from "util/queryKeys";
+import { useInstanceLoading } from "context/instanceLoading";
+import { useEventQueue } from "context/eventQueue";
+import { useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { useStoragePools } from "context/useStoragePools";
+import { useProjects } from "context/useProjects";
+import { useProjectEntitlements } from "util/entitlements/projects";
+
+export interface MigrateInstanceFormValues {
+  project: string;
+  location?: string;
+  storagePool: string;
+}
 
 interface Props {
   close: () => void;
@@ -18,13 +38,108 @@ interface Props {
 
 const MigrateInstanceModal: FC<Props> = ({ close, instance }) => {
   const isClustered = useIsClustered();
-  const [type, setType] = useState<MigrationType>("");
-  const [target, setTarget] = useState("");
-  const { handleMigrate } = useInstanceMigration({
-    close,
-    instance,
-    type,
-    target,
+  const toastNotify = useToastNotification();
+  const instanceLoading = useInstanceLoading();
+  const eventQueue = useEventQueue();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { data: projects = [], isLoading: projectsLoading } = useProjects();
+  const { data: storagePools = [], isLoading: storagePoolsLoading } =
+    useStoragePools();
+  const { canCreateInstances } = useProjectEntitlements();
+
+  const handleSuccess = (values: MigrateInstanceFormValues) => {
+    const oldUrl = window.location.pathname;
+    const newUrl = oldUrl.replace(
+      `/project/${instance.project}/instance/${instance.name}`,
+      `/project/${values.project}/instance/${instance.name}`,
+    );
+    // we are still on the instance detail page and the project has changed
+    if (oldUrl !== newUrl) {
+      navigate(newUrl);
+    }
+
+    const newInstance = {
+      ...instance,
+      project: values.project,
+    };
+    toastNotify.success(
+      <>
+        Instance <InstanceLinkChip instance={newInstance} /> successfully
+        migrated.
+      </>,
+    );
+  };
+
+  const handleFailure = (e: Error) => {
+    toastNotify.failure(
+      `Migration failed for instance ${instance.name}`,
+      e,
+      <InstanceLinkChip instance={instance} />,
+    );
+  };
+
+  const clearCache = () => {
+    queryClient.invalidateQueries({
+      queryKey: [queryKeys.instances, instance.name],
+    });
+    instanceLoading.setFinish(instance);
+  };
+
+  const formik = useFormik<MigrateInstanceFormValues>({
+    initialValues: {
+      project: instance.project,
+      location: isClustered ? instance.location : "",
+      storagePool:
+        (instance.devices["root"] as LxdDiskDevice)?.pool ??
+        storagePools[0]?.name,
+    },
+    enableReinitialize: true,
+    onSubmit: (values) => {
+      instanceLoading.setLoading(instance, "Migrating");
+      const targetPool =
+        formik.values.storagePool !== formik.initialValues.storagePool
+          ? formik.values.storagePool
+          : undefined;
+      const targetProject =
+        formik.values.project !== formik.initialValues.project
+          ? formik.values.project
+          : undefined;
+      migrateInstance(
+        instance.name,
+        instance.project,
+        values.location,
+        targetPool,
+        targetProject,
+      )
+        .then((operation) => {
+          toastNotify.info(
+            <>
+              Migration started for <InstanceLinkChip instance={instance} />.
+            </>,
+          );
+          queryClient.invalidateQueries({
+            queryKey: [queryKeys.instances, instance.name, instance.project],
+          });
+          eventQueue.set(
+            operation.metadata.id,
+            () => {
+              handleSuccess(values);
+            },
+            (msg) => {
+              handleFailure(new Error(msg));
+            },
+            clearCache,
+          );
+        })
+        .catch((e: Error) => {
+          handleFailure(e);
+        })
+        .finally(() => {
+          close();
+          clearCache();
+        });
+    },
   });
 
   const handleEscKey = (e: KeyboardEvent<HTMLElement>) => {
@@ -33,116 +148,73 @@ const MigrateInstanceModal: FC<Props> = ({ close, instance }) => {
     }
   };
 
-  const handleGoBack = () => {
-    // if target is set, we are on the confirmation stage
-    if (target) {
-      setTarget("");
-      return;
-    }
-
-    // if type is set, we are on migration target selection stage
-    if (type) {
-      setType("");
-      return;
-    }
-  };
-
-  const selectStepTitle = (
-    <>
-      Choose {type} for instance <strong>{instance.name}</strong>
-    </>
-  );
-
-  const modalTitle = !type ? (
-    "Choose migration method"
-  ) : (
-    <BackLink
-      title={target ? "Confirm migration" : selectStepTitle}
-      onClick={handleGoBack}
-      linkText={target ? `Choose ${type}` : "Choose migration method"}
-    />
-  );
+  const hasNothingChanged =
+    formik.values.project === formik.initialValues.project &&
+    formik.values.location === formik.initialValues.location &&
+    formik.values.storagePool === formik.initialValues.storagePool;
 
   return (
     <Modal
       close={close}
       className="migrate-instance-modal"
+      title="Migrate instance"
       onKeyDown={handleEscKey}
       aria-labelledby="migrate-title"
+      buttonRow={
+        <>
+          <Button
+            appearance="base"
+            className="u-no-margin--bottom"
+            type="button"
+            onClick={close}
+          >
+            Cancel
+          </Button>
+          <ActionButton
+            appearance="positive"
+            className="u-no-margin--bottom"
+            loading={formik.isSubmitting}
+            disabled={
+              !formik.isValid ||
+              storagePoolsLoading ||
+              projectsLoading ||
+              hasNothingChanged
+            }
+            onClick={() => void formik.submitForm()}
+          >
+            Migrate instance
+          </ActionButton>
+        </>
+      }
     >
-      <header className="p-modal__header">
-        <h2
-          className="p-modal__title"
-          key={type ? (target ? "confirm" : "select") : "start"}
-          id="migrate-title"
-        >
-          {modalTitle}
-        </h2>
-        <button
-          className="p-modal__close"
-          aria-label="Close active modal"
-          onClick={close}
-        >
-          Close
-        </button>
-      </header>
-      {!type && (
-        <div className="choose-migration-type">
-          {isClustered && (
-            <FormLink
-              icon="cluster-host"
-              title="Migrate instance to a different cluster member"
-              onClick={() => {
-                setType("cluster member");
-              }}
-            />
-          )}
-          <FormLink
-            icon="switcher-dashboard"
-            title="Move instance root storage to a different pool"
-            onClick={() => {
-              setType("root storage pool");
-            }}
-          />
-          <FormLink
-            icon="folder"
-            title="Move instance to a different project"
-            onClick={() => {
-              setType("project");
-            }}
-          />
-        </div>
-      )}
-
-      {type === "cluster member" && (
-        <InstanceClusterMemberMigration
-          instance={instance}
-          onSelect={setTarget}
-          targetMember={target}
-          onCancel={handleGoBack}
-          migrate={handleMigrate}
+      <Form onSubmit={formik.handleSubmit}>
+        <ClusterMemberSelector
+          {...formik.getFieldProps("location")}
+          id="location"
+          label="Cluster member"
         />
-      )}
-
-      {type === "root storage pool" && (
-        <InstanceStoragePoolMigration
-          instance={instance}
-          onSelect={setTarget}
-          targetPool={target}
-          onCancel={handleGoBack}
-          migrate={handleMigrate}
+        <StoragePoolSelector
+          {...formik.getFieldProps("storagePool")}
+          setValue={(value) => void formik.setFieldValue("storagePool", value)}
+          selectProps={{
+            id: "storagePool",
+            label: "Storage pool",
+          }}
         />
-      )}
-
-      {type === "project" && (
-        <InstanceProjectMigration
-          instance={instance}
-          onSelect={setTarget}
-          targetProject={target}
-          onCancel={handleGoBack}
-          migrate={handleMigrate}
+        <Select
+          {...formik.getFieldProps("project")}
+          id="project"
+          label="Project"
+          options={projects.filter(canCreateInstances).map((project) => {
+            return {
+              label: project.name,
+              value: project.name,
+            };
+          })}
         />
-      )}
+        {/* hidden submit to enable enter key in inputs */}
+        <Input type="submit" hidden value="Hidden input" />
+      </Form>
     </Modal>
   );
 };
