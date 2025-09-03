369864567a31294fa259d9da7d40549f4d5850f7
Nkeiruka <nkeiruka.whenu@canonical.com>
2025-09-03 16:47:51 +0100

    feat: [WD-26013] Hide acknowledged warnings by default
    Signed-off-by: Nkeiruka <nkeiruka.whenu@canonical.com>

--- src/types/warning.d.ts
+++ src/types/warning.d.ts
@@ -1,5 +1,5 @@
 export interface LxdWarning {
-  status: string;
+  status: LxdWarningStatus;
   uuid: string;
   project: string;
   type: string;
@@ -7,6 +7,9 @@ export interface LxdWarning {
   first_seen_at: string;
   last_seen_at: string;
   last_message: string;
-  severity: string;
+  severity: LxdWarningSeverity;
   access_entitlements?: string[];
 }
+
+export type LxdWarningStatus = "new" | "acknowledged" | "resolved";
+export type LxdWarningSeverity = "low" | "moderate" | "high";
