dc247dfe245e19954864cdaf2b47adb317def1a1
David Edler <david.edler@canonical.com>
2025-09-03 14:22:03 +0200

    fix(microcloud) set page title to microcloud if it applies. fixes #1461
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/util/title.tsx
+++ src/util/title.tsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
 import { useSettings } from "context/useSettings";
+import { hasMicroCloudFlag } from "util/settings";
 
 export const setTitle = (): void => {
   const { data: settings } = useSettings();
+  const isMicroCloud = hasMicroCloudFlag(settings);
+  const suffix = isMicroCloud ? "MicroCloud" : "LXD UI";
 
   useEffect(() => {
     const host = settings?.config?.["user.ui_title"] ?? location.hostname;
-    document.title = `${host} | LXD UI`;
+    document.title = `${host} | ${suffix}`;
   }, [settings?.config]);
 };
