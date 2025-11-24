74353a783588f73d1a79f882491258a98e8b8c18
Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>
2025-11-24 15:29:52 +0100

    feat(DeleteInstanceBtn): force stop and delete running instance
    Signed-off-by: Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>

--- src/util/instanceStatus.tsx
+++ src/util/instanceStatus.tsx
@@ -3,3 +3,7 @@ import type { LxdInstance } from "types/instance";
 export const isInstanceRunning = (instance: LxdInstance) => {
   return ["Ready", "Running"].includes(instance.status);
 };
+
+export const isInstanceFrozen = (instance: LxdInstance) => {
+  return instance.status === "Frozen";
+};
