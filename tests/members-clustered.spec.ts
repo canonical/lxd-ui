8338106a5d2f7a12f6f13b95da3e4f3cd5af3bd2
Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>
2026-04-28 10:54:02 +0200

    feat(cluster member): add disable reason for evacuate & restore buttons
    Signed-off-by: Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>

--- tests/members-clustered.spec.ts
+++ tests/members-clustered.spec.ts
@@ -9,26 +9,40 @@ test("cluster member evacuate and restore", async ({
 }, testInfo) => {
   skipIfNotSupported(lxdVersion);
   skipIfNotClustered(testInfo.project.name);
+
   const member = await getFirstClusterMember(page);
   const memberRow = page.getByRole("row").filter({ hasText: member });
 
   await memberRow.hover();
   const restoreBtn = memberRow.getByRole("button", { name: "Restore" });
+
   if (await restoreBtn.isEnabled()) {
     await restoreBtn.click();
-    await page.getByText("Restore cluster member", { exact: true }).click();
+    await page
+      .getByRole("dialog", { name: "Confirm restore" })
+      .getByRole("button", { name: "Restore cluster member" })
+      .click();
     await dismissNotification(page, `Member ${member} restore completed.`);
   }
 
   await memberRow.hover();
-  await memberRow.getByRole("button", { name: "Evacuate" }).click();
-  await page.getByText("Evacuate cluster member", { exact: true }).click();
-
+  await memberRow
+    .getByRole("button", { name: "Evacuate cluster member" })
+    .click();
+  await page
+    .getByRole("dialog", {
+      name: "Confirm evacuation",
+    })
+    .getByRole("button", { name: "Evacuate cluster member" })
+    .click();
   await dismissNotification(page, `Member ${member} evacuation completed.`);
 
   await memberRow.hover();
   await memberRow.getByRole("button", { name: "Restore" }).click();
-  await page.getByText("Restore cluster member", { exact: true }).click();
+  await page
+    .getByRole("dialog", { name: "Confirm restore" })
+    .getByRole("button", { name: "Restore cluster member" })
+    .click();
 
   await dismissNotification(page, `Member ${member} restore completed.`);
 });
