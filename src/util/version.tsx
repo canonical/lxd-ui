0413fc5b563e7c44659cc6e9da24f4eab32cfd00
David Edler <david.edler@canonical.com>
2026-06-17 19:46:45 +0200

    chore(version) update to 0.22
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/util/version.tsx
+++ src/util/version.tsx
@@ -1,5 +1,5 @@
 export const RECENT_MAJOR_SERVER_VERSION = 5;
-export const UI_VERSION = "0.21";
+export const UI_VERSION = "0.22";
 
 // defined in vite.config.ts and injected at build time
 declare const __UI_GIT_HASH__: string;
