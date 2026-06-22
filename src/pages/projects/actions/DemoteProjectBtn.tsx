578441b18b7f2ce1d817df29e7d2b0a06bc981a2
Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>
2026-06-22 17:30:03 +0200

    fix(replicator): force flag for promote and demote project
    Signed-off-by: Kim Anh Nguyen <4783194+kimanhou@users.noreply.github.com>

--- src/pages/projects/actions/DemoteProjectBtn.tsx
+++ src/pages/projects/actions/DemoteProjectBtn.tsx
@@ -1,9 +1,9 @@
 import {
-  ActionButton,
-  useNotify,
+  ConfirmationButton,
   useToastNotification,
 } from "@canonical/react-components";
 import { type FC, useState } from "react";
+import ConfirmationCheckbox from "components/ConfirmationCheckbox";
 import { useIsScreenBelow } from "context/useIsScreenBelow";
 import ProjectRichChip from "pages/projects/ProjectRichChip";
 import { useProjectEntitlements } from "util/entitlements/projects";
@@ -17,9 +17,9 @@ interface Props {
 const DemoteProjectBtn: FC<Props> = ({ project, isEdit }: Props) => {
   const [isDemoting, setIsDemoting] = useState(false);
   const isSmallScreen = useIsScreenBelow();
-  const notify = useNotify();
   const toastNotify = useToastNotification();
   const { canEditProject } = useProjectEntitlements();
+  const [isForce, setForce] = useState(false);
 
   const disabledReason = () => {
     if (!canEditProject) {
@@ -42,35 +42,54 @@ const DemoteProjectBtn: FC<Props> = ({ project, isEdit }: Props) => {
   };
 
   const handleFailure = (e: unknown) => {
-    notify.failure("Could not demote project to standby.", e as Error);
+    toastNotify.failure("Could not demote project to standby.", e as Error);
   };
 
   const demoteToStandby = () => {
     setIsDemoting(true);
-    updateReplicaMode(project, "standby", handleSuccess, handleFailure).finally(
-      () => {
-        setIsDemoting(false);
-      },
-    );
+    updateReplicaMode(
+      project,
+      "standby",
+      handleSuccess,
+      handleFailure,
+      isForce,
+    ).finally(() => {
+      setIsDemoting(false);
+    });
   };
 
   return (
-    <ActionButton
+    <ConfirmationButton
       type="button"
       name="Demote to standby"
       hasIcon
       appearance="default"
       onClick={demoteToStandby}
       loading={isDemoting}
-      title={
+      onHoverText={
         disabledReason() ??
         "Project will become the read-only replication target."
       }
       className="u-no-margin--bottom"
       disabled={Boolean(disabledReason())}
+      confirmationModalProps={{
+        title: "Confirm demote",
+        children: (
+          <p>
+            This will demote project <ProjectRichChip projectName={project} />{" "}
+            to standby.
+          </p>
+        ),
+        confirmButtonLabel: "Demote",
+        onConfirm: demoteToStandby,
+        confirmExtra: (
+          <ConfirmationCheckbox label="Force" confirmed={[isForce, setForce]} />
+        ),
+        confirmButtonAppearance: "positive",
+      }}
     >
       <span>{isSmallScreen ? "Demote" : "Demote to standby"}</span>
-    </ActionButton>
+    </ConfirmationButton>
   );
 };
 
