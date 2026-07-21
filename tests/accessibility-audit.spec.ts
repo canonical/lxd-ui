a40f65806591b2edc3d489c8bf504c06531fa841
Nkeiruka <nkeiruka.whenu@canonical.com>
2026-07-21 01:34:14 +0100

    fix(a11y tests) - Fine tuning to disallow tests for non-supported entities in 5.21 and below
    Signed-off-by: Nkeiruka <nkeiruka.whenu@canonical.com>

--- tests/accessibility-audit.spec.ts
+++ tests/accessibility-audit.spec.ts
@@ -23,6 +23,18 @@ test("accessibility audit for storage pages", async ({ page }) => {
   await runA11yAudit("Pools", page, test.info(), slug);
   await runA11yAudit("Volumes", page, test.info(), slug);
   await runA11yAudit("Buckets", page, test.info(), slug);
+});
+
+test("accessibility audit for custom ISOs page", async ({
+  page,
+  lxdVersion,
+}) => {
+  test.skip(
+    lxdVersion === "5.0-edge",
+    "Custom ISOs are not supported in lxd 5.0",
+  );
+
+  const slug = "Storage";
   await runA11yAudit("Custom ISOs", page, test.info(), slug);
 });
 
@@ -40,8 +52,20 @@ test("accessibility audit for clustering pages", async ({ page }) => {
   const slug = "Clustering";
   await runA11yAudit("Server", page, test.info(), slug);
   await runA11yAudit("Groups", page, test.info(), slug);
-  await runA11yAudit("Links", page, test.info(), slug);
   await runA11yAudit("Placement", page, test.info(), slug);
+});
+
+test("accessibility audit for replicator and cluster link pages", async ({
+  page,
+  lxdVersion,
+}) => {
+  test.skip(
+    lxdVersion === "5.21-edge" || lxdVersion === "5.0-edge",
+    "Cluster Links & Replicators are not supported in older LXD versions",
+  );
+
+  const slug = "Clustering";
+  await runA11yAudit("Links", page, test.info(), slug);
   await runA11yAudit("Replicators", page, test.info(), slug);
 });
 
@@ -55,7 +79,15 @@ test("accessibility audit for warning page", async ({ page }) => {
   await runA11yAudit(slug, page, test.info());
 });
 
-test("accessibility audit for permissions pages", async ({ page }) => {
+test("accessibility audit for permissions pages", async ({
+  page,
+  lxdVersion,
+}) => {
+  test.skip(
+    lxdVersion === "5.0-edge",
+    "Fine grained authorisation is supported in lxd 5.21 LTS and latest/edge",
+  );
+
   const slug = "Permissions";
   await runA11yAudit("Identities", page, test.info(), slug);
   await runA11yAudit("Groups", page, test.info(), slug);
