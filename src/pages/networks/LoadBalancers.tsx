0307d80632c53ea760904dba998c51158ca80a65
David Edler <david.edler@canonical.com>
2026-09-02 16:32:25 +0200

    fix(network) disable load balancers on custom projects without network isolation
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/pages/networks/LoadBalancers.tsx
+++ src/pages/networks/LoadBalancers.tsx
@@ -2,25 +2,46 @@ import type { FC } from "react";
 import type { LxdNetwork } from "types/network";
 import { useCurrentProject } from "context/useCurrentProject";
 import MenuItem from "components/forms/FormMenuItem";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { ROOT_PATH } from "util/rootPath";
 import LoadBalancersTab from "pages/networks/LoadBalancersTab";
 import LoadBalancerPoolsTab from "pages/networks/LoadBalancerPoolsTab";
 import usePanelParams, { panels } from "util/usePanelParams";
 import CreateLoadBalancerPoolPanel from "pages/networks/panels/CreateLoadBalancerPoolPanel";
 import EditLoadBalancerPoolPanel from "pages/networks/panels/EditLoadBalancerPoolPanel";
+import { Notification } from "@canonical/react-components";
+import ProjectRichChip from "pages/projects/ProjectRichChip";
 
 interface Props {
   network: LxdNetwork;
 }
 
 const LoadBalancers: FC<Props> = ({ network }) => {
-  const { projectName: project } = useCurrentProject();
+  const { projectName, project, isLoading } = useCurrentProject();
   const { section } = useParams<{
     section?: string;
   }>();
   const navigate = useNavigate();
   const panelParams = usePanelParams();
+
+  if (
+    projectName !== "default" &&
+    project?.config["features.networks"] !== "true" &&
+    !isLoading
+  ) {
+    return (
+      <Notification severity="caution" title="Network isolation is disabled">
+        The current project <ProjectRichChip projectName={projectName} /> has
+        disabled network isolation. Please enable network isolation in{" "}
+        <Link
+          to={`${ROOT_PATH}/ui/project/${encodeURIComponent(projectName)}/configuration`}
+        >
+          project configuration
+        </Link>{" "}
+        to access load balancer features.
+      </Notification>
+    );
+  }
 
   return (
     <>
@@ -33,7 +54,7 @@ const LoadBalancers: FC<Props> = ({ network }) => {
                 active={section ?? "load-balancers"}
                 setActive={() => {
                   navigate(
-                    `${ROOT_PATH}/ui/project/${encodeURIComponent(project)}/network/${encodeURIComponent(network.name)}/load-balancers`,
+                    `${ROOT_PATH}/ui/project/${encodeURIComponent(projectName)}/network/${encodeURIComponent(network.name)}/load-balancers`,
                   );
                 }}
               />
@@ -42,7 +63,7 @@ const LoadBalancers: FC<Props> = ({ network }) => {
                 active={section ?? ""}
                 setActive={() => {
                   navigate(
-                    `${ROOT_PATH}/ui/project/${encodeURIComponent(project)}/network/${encodeURIComponent(network.name)}/load-balancers/pools`,
+                    `${ROOT_PATH}/ui/project/${encodeURIComponent(projectName)}/network/${encodeURIComponent(network.name)}/load-balancers/pools`,
                   );
                 }}
               />
